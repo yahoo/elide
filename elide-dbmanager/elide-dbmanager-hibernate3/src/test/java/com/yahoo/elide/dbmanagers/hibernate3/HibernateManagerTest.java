70a7b494df3779428b6a8bb9e80654f2532bbd18
Daniel Chen <danchen@yahoo-inc.com>
2015-10-21 16:47:32 -0500

    Fix test dir for hibernate3

--- elide-dbmanager/elide-dbmanager-hibernate3/src/test/java/com/yahoo/elide/dbmanagers/hibernate3/HibernateManagerTest.java
+++ elide-dbmanager/elide-dbmanager-hibernate3/src/test/java/com/yahoo/elide/dbmanagers/hibernate3/HibernateManagerTest.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2015, Yahoo Inc.
+ * Licensed under the Apache License, Version 2.0
+ * See LICENSE file in project root for terms.
+ */
 package com.yahoo.elide.dbmanagers.hibernate3;
 
 import org.testng.annotations.Test;
