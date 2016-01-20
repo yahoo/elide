084ad35903949a4818148f49aabddbddb2cc405b
Shad Sharma <shad@yahoo-inc.com>
2016-01-19 18:53:19 -0600

    make maps work

---
+++ elide-integration-tests/src/test/groovy/com/yahoo/elide/tests/MapEnumIT.groovy
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2015, Yahoo Inc.
+ * Licensed under the Apache License, Version 2.0
+ * See LICENSE file in project root for terms.
+ */
+package com.yahoo.elide.tests
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.yahoo.elide.core.HttpStatus
+import com.yahoo.elide.initialization.AbstractIntegrationTestInitializer
+import org.testng.Assert
+import org.testng.annotations.Test
+
+import static com.jayway.restassured.RestAssured.given
+
+/**
+ * Test rehydration of Map of Enums.
+ */
+class MapEnumIT extends AbstractIntegrationTestInitializer {
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    @Test
+    public void testPostColorShape() {
+        // Create MapColorShape using Post
+        def postRequest = given()
+                .contentType("application/vnd.api+json")
+                .accept("application/vnd.api+json")
+                .body('''
+                    {
+                      "data": {
+                        "type": "mapColorShape",
+                        "attributes": {
+                          "colorShapeMap": {
+                            "Red": "Circle"
+                          }
+                        }
+                      }
+                    }
+                    ''')
+                .post("/mapColorShape")
+        postRequest.then().statusCode(HttpStatus.SC_CREATED)
+
+        def postResponse = mapper.readTree(postRequest.asString())
+        Assert.assertEquals(postResponse["data"]["attributes"]["colorShapeMap"]["Red"].asText(), "Circle")
+
+        // Update MapColorShape using Patch
+        def patchRequest = given()
+                .contentType("application/vnd.api+json")
+                .accept("application/vnd.api+json")
+                .body("""
+                      {
+                        "data": {
+                          "id": ${postResponse['data']['id']},
+                          "type": "mapColorShape",
+                          "attributes": {
+                            "colorShapeMap": {
+                              "Blue": "Square"
+                            }
+                          }
+                        }
+                      }
+                    ]
+                    """)
+                .patch("/mapColorShape/${postResponse['data']['id'].asText()}/")
+        patchRequest.then().statusCode(HttpStatus.SC_NO_CONTENT)
+
+        // Get MapColorShape
+        def getRequest = given()
+                .accept("application/vnd.api+json")
+                .get("/mapColorShape/${postResponse['data']['id'].asText()}/")
+        def getResponse = mapper.readTree(getRequest.asString())
+        Assert.assertEquals(getResponse["data"]["attributes"]["colorShapeMap"]["Blue"].asText(), "Square")
+    }
+
+    @Test
+    public void testPatchExtensionColorShape() {
+        // Create MapColorShape using Patch extension
+        def request = given()
+                .contentType("application/vnd.api+json; ext=jsonpatch")
+                .accept("application/vnd.api+json; ext=jsonpatch")
+                .body('''
+                    [
+                      {
+                        "op": "add",
+                        "path": "/mapColorShape",
+                        "value": {
+                          "id": "12345681-1234-1234-1234-1234567890ab",
+                          "type": "mapColorShape",
+                          "attributes": {
+                            "colorShapeMap": {
+                              "Blue": "Triangle"
+                            }
+                          }
+                        }
+                      }
+                    ]
+                    ''')
+                .patch("/")
+        request.then().statusCode(HttpStatus.SC_OK)
+
+        def response = mapper.readTree(request.asString())
+        Assert.assertEquals(response[0]["data"]["attributes"]["colorShapeMap"]["Blue"].asText(), "Triangle")
+    }
+}
---
+++ elide-integration-tests/src/test/java/example/MapColorShape.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2015, Yahoo Inc.
+ * Licensed under the Apache License, Version 2.0
+ * See LICENSE file in project root for terms.
+ */
+package example;
+
+import com.yahoo.elide.annotation.Include;
+import com.yahoo.elide.annotation.SharePermission;
+import com.yahoo.elide.security.Role;
+
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MapKeyColumn;
+import javax.persistence.Table;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Bean with Map of Enums.
+ */
+@Entity
+@Table(name = "color_shape")
+@Include(rootLevel = true)
+@SharePermission(any = {Role.ALL.class})
+public class MapColorShape {
+    private long id;
+    private Map<Color, Shape> colorShapeMap = new LinkedHashMap<>();
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @ElementCollection
+    @MapKeyColumn(name = "color")
+    @Column(name = "shape")
+    public Map<Color, Shape> getColorShapeMap() {
+        return colorShapeMap;
+    }
+
+    public void setColorShapeMap(Map<Color, Shape> colorShapeMap) {
+        this.colorShapeMap = colorShapeMap;
+    }
+}
