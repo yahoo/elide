50fce8c9bf69480c1f58a41aaca38cf8a80ae147
William Cekan <wcekan@yahoo-inc.com>
2016-03-04 06:33:56 -0600

    Run DataStore specific integration tests

---
+++ elide-integration-tests/src/test/java/com/yahoo/elide/tests/DataStoreIT.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2015, Yahoo Inc.
+ * Licensed under the Apache License, Version 2.0
+ * See LICENSE file in project root for terms.
+ */
+package com.yahoo.elide.tests;
+
+/**
+ * Placeholder for additional Datastore tests.
+ */
+public class DataStoreIT {
+}
