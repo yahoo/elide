f61608f347301404e92af9571924bae8813a1d0b
Will Koster <jameswk2@illinois.edu>
2016-12-14 11:34:22 -0600

    Add license headers

--- elide-contrib/elide-swagger/src/com/yahoo/elide/contrib/swagger/JSONObjectClasses/MimeType.java
+++ elide-contrib/elide-swagger/src/com/yahoo/elide/contrib/swagger/JSONObjectClasses/MimeType.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2016, Yahoo Inc.
+ * Licensed under the Apache License, Version 2.0
+ * See LICENSE file in project root for terms.
+ */
 package com.yahoo.elide.contrib.swagger.JSONObjectClasses;
 
 /*
