46cde97249da1eb409376ca94ac405fd2fb38279
klish <aklish@gmail.com>
2017-02-20 22:21:06 -0600

    Add input type generation and general cleanup

--- elide-core/src/main/java/com/yahoo/elide/graphql/ModelBuilder.java
+++ elide-core/src/main/java/com/yahoo/elide/graphql/ModelBuilder.java
@@ -5,33 +5,35 @@ import com.yahoo.elide.core.RelationshipType;
 import graphql.Scalars;
 import graphql.schema.DataFetcher;
 import graphql.schema.GraphQLArgument;
+import graphql.schema.GraphQLInputObjectType;
+import graphql.schema.GraphQLInputType;
 import graphql.schema.GraphQLList;
 import graphql.schema.GraphQLObjectType;
 import graphql.schema.GraphQLOutputType;
 import graphql.schema.GraphQLSchema;
+import graphql.schema.GraphQLType;
 import graphql.schema.GraphQLTypeReference;
 
 import java.util.ArrayDeque;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Queue;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import static graphql.schema.GraphQLFieldDefinition.newFieldDefinition;
+import static graphql.schema.GraphQLInputObjectField.newInputObjectField;
+import static graphql.schema.GraphQLInputObjectType.newInputObject;
 import static graphql.schema.GraphQLObjectType.newObject;
 
 public class ModelBuilder {
     private EntityDictionary dictionary;
-    private DataFetcher queryFetcher;
-    private DataFetcher mutationFetcher;
+    private DataFetcher dataFetcher;
     private GraphQLArgument relationshipOpArg;
     private GraphQLArgument idArgument;
 
-    ModelBuilder(EntityDictionary dictionary, DataFetcher queryFetcher, DataFetcher mutationFetcher) {
+    ModelBuilder(EntityDictionary dictionary, DataFetcher dataFetcher) {
         this.dictionary = dictionary;
-        this.queryFetcher = queryFetcher;
-        this.mutationFetcher = mutationFetcher;
+        this.dataFetcher = dataFetcher;
 
         relationshipOpArg = GraphQLArgument.newArgument()
                 .name("op")
@@ -45,7 +47,11 @@ public class ModelBuilder {
                 .build();
     }
 
-    GraphQLSchema build() {
+    /**
+     * Builds a GraphQL schema.
+     * @return The built schema.
+     */
+    public GraphQLSchema build() {
         Set<Class<?>> allClasses = dictionary.getBindings();
         if (allClasses.isEmpty()) {
             throw new IllegalArgumentException("None of the provided classes are exported by Elide");
@@ -59,13 +65,17 @@ public class ModelBuilder {
             String entityName = dictionary.getJsonAliasFor(clazz);
             root.field(newFieldDefinition()
                     .name(entityName)
-                    .dataFetcher(queryFetcher)
+                    .dataFetcher(dataFetcher)
                     .argument(relationshipOpArg)
                     .argument(idArgument)
+                    .argument(buildInputObjectArgument(clazz, true))
                     .type(new GraphQLList(buildQueryObject(clazz))));
         }
 
 
+        /*
+         * Search the object graph, avoid cycles, and construct the GraphQL output object types.
+         */
         Queue < Class < ?>> toVisit = new ArrayDeque<>(rootClasses);
         while (! toVisit.isEmpty()) {
             Class<?> clazz = toVisit.remove();
@@ -87,31 +97,32 @@ public class ModelBuilder {
         return schema;
     }
 
-    GraphQLObjectType buildQueryObject(Class<?> entityClass) {
+    /**
+     * Builds a graphQL output object from an entity class.
+     * @param entityClass The class to use to construct the output object.
+     * @return The graphQL object
+     */
+    private GraphQLObjectType buildQueryObject(Class<?> entityClass) {
         String entityName = dictionary.getJsonAliasFor(entityClass);
 
         GraphQLObjectType.Builder builder = newObject();
         builder.name(entityName);
-
-        List<String> attributes = dictionary.getAttributes(entityClass);
 
         String id = dictionary.getIdFieldName(entityClass);
         builder.field(newFieldDefinition()
                     .name(id)
                     .type(Scalars.GraphQLID));
 
-        for (String attribute : attributes) {
+        for (String attribute : dictionary.getAttributes(entityClass)) {
             Class<?> attributeClass = dictionary.getType(entityClass, attribute);
             builder.field(newFieldDefinition()
                     .name(attribute)
-                    .dataFetcher(queryFetcher)
-                    .type(classToType(attributeClass))
+                    .dataFetcher(dataFetcher)
+                    .type((GraphQLOutputType) classToType(attributeClass))
             );
         }
 
-        List<String> relationships = dictionary.getRelationships(entityClass);
-
-        for (String relationship : relationships) {
+        for (String relationship : dictionary.getRelationships(entityClass)) {
             Class<?> relationshipClass = dictionary.getType(entityClass, relationship);
 
             String relationshipEntityName = dictionary.getJsonAliasFor(relationshipClass);
@@ -120,16 +131,18 @@ public class ModelBuilder {
             if (type.isToOne()) {
                 builder.field(newFieldDefinition()
                                 .name(relationship)
-                                .dataFetcher(queryFetcher)
+                                .dataFetcher(dataFetcher)
                                 .argument(relationshipOpArg)
+                                .argument(buildInputObjectArgument(relationshipClass, false))
                                 .type(new GraphQLTypeReference(relationshipEntityName))
                 );
             } else {
                 builder.field(newFieldDefinition()
                                 .name(relationship)
-                                .dataFetcher(queryFetcher)
+                                .dataFetcher(dataFetcher)
                                 .argument(relationshipOpArg)
                                 .argument(idArgument)
+                                .argument(buildInputObjectArgument(relationshipClass, true))
                                 .type(new GraphQLList(new GraphQLTypeReference(relationshipEntityName)))
                 );
             }
@@ -138,8 +151,86 @@ public class ModelBuilder {
         return builder.build();
     }
 
+    /**
+     * Wraps a constructed GraphQL Input Object in an argument.
+     * @param entityClass - The class to construct the input object from.
+     * @param asList Whether or not the argument is a single instance or a list.
+     * @return The constructed argument.
+     */
+    private GraphQLArgument buildInputObjectArgument(Class<?> entityClass, boolean asList) {
+        GraphQLInputObjectType argumentType = buildInputObject(entityClass, new HashSet<GraphQLInputObjectType>());
 
-    GraphQLOutputType classToType(Class<?> clazz) {
+        if (asList) {
+             return GraphQLArgument.newArgument()
+                    .name("relationship")
+                    .type(new GraphQLList(argumentType))
+                    .build();
+        } else {
+            return GraphQLArgument.newArgument()
+                    .name("relationship")
+                    .type(argumentType)
+                    .build();
+        }
+    }
+
+    /**
+     * Builds a nested GraphQL input object that traverses relationships but avoids cycles.
+     * @param entityClass The starting entity type to build the input object from.
+     * @param visited Entities already visited
+     * @return Constructed graphql input object type.
+     */
+    private GraphQLInputObjectType buildInputObject(Class<?> entityClass, Set<GraphQLInputObjectType> visited) {
+        String entityName = dictionary.getJsonAliasFor(entityClass);
+
+        GraphQLInputObjectType.Builder builder = newInputObject();
+        builder.name(entityName);
+
+        String id = dictionary.getIdFieldName(entityClass);
+
+        builder.field(newInputObjectField()
+                .name(id)
+                .type(Scalars.GraphQLID));
+
+        for (String attribute : dictionary.getAttributes(entityClass)) {
+            Class<?> attributeClass = dictionary.getType(entityClass, attribute);
+            builder.field(newInputObjectField()
+                            .name(attribute)
+                            .type((GraphQLInputType) classToType(attributeClass))
+            );
+        }
+
+        for (String relationship : dictionary.getRelationships(entityClass)) {
+            Class<?> relationshipClass = dictionary.getType(entityClass, relationship);
+
+            RelationshipType type = dictionary.getRelationshipType(entityClass, relationship);
+
+            //TODO - this is inadequate cycle protection but should cover the common cases for now.
+            if (visited.contains(relationshipClass)) {
+                continue;
+            }
+
+            if (type.isToOne()) {
+                builder.field(newInputObjectField()
+                                .name(relationship)
+                                .type(buildInputObject(relationshipClass, visited))
+                );
+            } else {
+                builder.field(newInputObjectField()
+                                .name(relationship)
+                                .type(new GraphQLList(buildInputObject(relationshipClass, visited)))
+                );
+            }
+        }
+
+        return builder.build();
+    }
+
+    /**
+     * Converts any non-entity type to a GraphQLType
+     * @param clazz - the non-entity type.
+     * @return the GraphQLType
+     */
+    private GraphQLType classToType(Class<?> clazz) {
         if (clazz.equals(int.class) || clazz.equals(Integer.class)) {
             return Scalars.GraphQLInt;
         } else if (clazz.equals(boolean.class) || clazz.equals(Boolean.class)) {
@@ -153,6 +244,8 @@ public class ModelBuilder {
         } else if (clazz.equals(String.class)) {
             return Scalars.GraphQLString;
         }
+
+        //TODO - handle collections & embedded entities
 
         return null;
     }
