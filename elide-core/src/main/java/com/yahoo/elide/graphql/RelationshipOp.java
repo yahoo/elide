4bbe42d059e0f17968988bbd50671649d36910be
klish <aklish@gmail.com>
2017-02-22 16:37:08 -0600

    Fixed checkstyle errors

--- elide-core/src/main/java/com/yahoo/elide/graphql/RelationshipOp.java
+++ elide-core/src/main/java/com/yahoo/elide/graphql/RelationshipOp.java
@@ -1,15 +1,28 @@
+/*
+ * Copyright 2017, Yahoo Inc.
+ * Licensed under the Apache License, Version 2.0
+ * See LICENSE file in project root for terms.
+ */
+
 package com.yahoo.elide.graphql;
 
 import graphql.schema.GraphQLEnumType;
 
 import static graphql.schema.GraphQLEnumType.newEnum;
 
+/**
+ * Specifies how a relationship should be modified.
+ */
 public enum RelationshipOp {
     FETCH,
     DELETE,
     ADD,
     REPLACE;
 
+    /**
+     * Converts this enum to a graphQL schema type.
+     * @return graphql enum type
+     */
     public static GraphQLEnumType toGraphQLType() {
         GraphQLEnumType.Builder builder = newEnum()
                 .name("op")
