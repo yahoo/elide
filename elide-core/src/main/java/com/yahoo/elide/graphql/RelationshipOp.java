13a73ad2c34e8132778acca74a5114f53e1029b9
klish <aklish@gmail.com>
2017-02-28 15:49:41 -0600

    Added enum support

--- elide-core/src/main/java/com/yahoo/elide/graphql/RelationshipOp.java
+++ elide-core/src/main/java/com/yahoo/elide/graphql/RelationshipOp.java
@@ -6,10 +6,6 @@
 
 package com.yahoo.elide.graphql;
 
-import graphql.schema.GraphQLEnumType;
-
-import static graphql.schema.GraphQLEnumType.newEnum;
-
 /**
  * Specifies how a relationship should be modified.
  */
@@ -18,20 +14,4 @@ public enum RelationshipOp {
     DELETE,
     ADD,
     REPLACE;
-
-    /**
-     * Converts this enum to a graphQL schema type.
-     * @return graphql enum type
-     */
-    public static GraphQLEnumType toGraphQLType() {
-        GraphQLEnumType.Builder builder = newEnum()
-                .name("op")
-                .description("Relationship operations.");
-
-        for (RelationshipOp op : RelationshipOp.values()) {
-            builder.value(op.name(), op);
-        }
-
-        return builder.build();
-    }
 }
