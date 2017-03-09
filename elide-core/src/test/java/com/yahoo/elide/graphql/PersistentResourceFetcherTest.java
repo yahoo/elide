0396f5e3040ebc4fe276fca6da049212feb1c77e
Clay Reimann <clayreimann@gmail.com>
2017-03-09 12:40:23 -0600

    Add test for illumination

--- elide-core/src/test/java/com/yahoo/elide/graphql/PersistentResourceFetcherTest.java
+++ elide-core/src/test/java/com/yahoo/elide/graphql/PersistentResourceFetcherTest.java
@@ -6,9 +6,29 @@
 
 package com.yahoo.elide.graphql;
 
+import graphql.ExecutionResult;
+import graphql.GraphQL;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 /**
  * Test our data fetcher.
  */
-public class PersistentResourceFetcherTest {
+public class PersistentResourceFetcherTest extends AbstractGraphQLTest {
+    protected GraphQL api;
 
+    @BeforeMethod
+    public void setup() {
+        PersistentResourceFetcher fetcher = new PersistentResourceFetcher();
+        ModelBuilder builder = new ModelBuilder(dictionary, fetcher);
+        api = new GraphQL(builder.build());
+    }
+
+    @Test
+    public void testFetchRootObject() {
+        String graphQLRequest = "{ book { title } }";
+        ExecutionResult result = api.execute(graphQLRequest);
+        Assert.assertEquals(result.getData(), "");
+    }
 }
