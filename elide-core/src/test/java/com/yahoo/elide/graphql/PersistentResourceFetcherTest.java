34bff340ddc63af8ba7361f785b7f34f81a652e7
Clay Reimann <clayreimann@gmail.com>
2017-03-09 12:27:05 -0600

    Skeleton data fetcher

---
+++ elide-core/src/test/java/com/yahoo/elide/graphql/PersistentResourceFetcherTest.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2017, Yahoo Inc.
+ * Licensed under the Apache License, Version 2.0
+ * See LICENSE file in project root for terms.
+ */
+
+package com.yahoo.elide.graphql;
+
+/**
+ * Test our data fetcher.
+ */
+public class PersistentResourceFetcherTest {
+
+}
