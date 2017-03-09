6131b1efcd9d4bccd8086bac09166cc82e64ad70
Clay Reimann <clayreimann@gmail.com>
2017-03-09 12:43:56 -0600

    checkpoint

---
+++ elide-core/src/main/java/com/yahoo/elide/graphql/GraphQLContext.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2017, Yahoo Inc.
+ * Licensed under the Apache License, Version 2.0
+ * See LICENSE file in project root for terms.
+ */
+package com.yahoo.elide.graphql;
+
+import com.yahoo.elide.core.DataStoreTransaction;
+import com.yahoo.elide.core.EntityDictionary;
+import com.yahoo.elide.core.PersistentResource;
+import com.yahoo.elide.security.User;
+import lombok.Getter;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/**
+ * The equivalent of {@link com.yahoo.elide.core.RequestScope} for GraphQL.
+ */
+public class GraphQLContext {
+    @Getter private final DataStoreTransaction transaction;
+    @Getter private final User user;
+    @Getter private final EntityDictionary dictionary;
+    @Getter private final Set<PersistentResource> newPersistentResources;
+    @Getter private final LinkedHashSet<PersistentResource> dirtyResources;
+
+
+}
