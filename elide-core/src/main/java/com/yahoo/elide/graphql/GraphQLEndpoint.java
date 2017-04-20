3a6ed5310c7e97561048d50cf702f0ac936ace6d
William Cekan <wcekan@yahoo-inc.com>
2017-04-20 09:25:59 -0500

    GraphQL Endpoint

---
+++ elide-core/src/main/java/com/yahoo/elide/graphql/GraphQLEndpoint.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2015, Yahoo Inc.
+ * Licensed under the Apache License, Version 2.0
+ * See LICENSE file in project root for terms.
+ */
+package com.yahoo.elide.graphql;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.yahoo.elide.Elide;
+import com.yahoo.elide.core.DataStoreTransaction;
+import com.yahoo.elide.core.RequestScope;
+import com.yahoo.elide.core.exceptions.HttpStatusException;
+import com.yahoo.elide.core.exceptions.InvalidEntityBodyException;
+import com.yahoo.elide.core.exceptions.TransactionException;
+import com.yahoo.elide.resources.DefaultOpaqueUserFunction;
+import com.yahoo.elide.security.User;
+import graphql.ExecutionResult;
+import graphql.GraphQL;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
+
+import java.io.IOException;
+import java.util.function.Function;
+
+/**
+ * Default endpoint/servlet for using Elide and JSONAPI.
+ */
+@Slf4j
+@Singleton
+@Produces(MediaType.TEXT_PLAIN)
+@Path("/graphQLx")
+public class GraphQLEndpoint {
+
+    private Elide elide;
+    private GraphQL api;
+    protected final Function<SecurityContext, Object> getUser;
+
+    @Inject
+    public GraphQLEndpoint(
+            @Named("elide") Elide elide,
+            @Named("elideUserExtractionFunction") DefaultOpaqueUserFunction getUser) {
+        log.error("Started ~~");
+        this.elide = elide;
+        this.getUser = getUser;
+        PersistentResourceFetcher fetcher = new PersistentResourceFetcher();
+        ModelBuilder builder = new ModelBuilder(elide.getElideSettings().getDictionary(), fetcher);
+        this.api = new GraphQL(builder.build());
+        throw new RuntimeException();
+    }
+
+    /**
+     * Create handler.
+     *
+     * @param securityContext security context
+     * @param graphQLDocument post data as jsonapi document
+     * @return response
+     */
+    @POST
+    @Consumes(MediaType.TEXT_PLAIN)
+    public Response post(
+            @Context SecurityContext securityContext,
+            String graphQLDocument) {
+        String path = "/";
+        boolean isVerbose = false;
+        try (DataStoreTransaction tx = elide.getDataStore().beginTransaction()) {
+            final User user = tx.accessUser(getUser.apply(securityContext));
+            RequestScope requestScope = new RequestScope(path, null, tx, user, null, elide.getElideSettings());
+            ExecutionResult result = api.execute(graphQLDocument, requestScope);
+            return Response.ok(elide.getMapper().getObjectMapper().writeValueAsString(result.getData())).build();
+        } catch (JsonProcessingException e) {
+            return buildErrorResponse(new InvalidEntityBodyException(graphQLDocument), isVerbose);
+        } catch (IOException e) {
+            return buildErrorResponse(new TransactionException(e), isVerbose);
+        }
+    }
+
+    private Response buildErrorResponse(HttpStatusException error, boolean isVerbose) {
+        return Response.status(error.getStatus())
+                .entity(isVerbose ? error.getVerboseErrorResponse() : error.getErrorResponse()).build();
+    }
+}
