4493768f3057a7548e884459375558f6690f0293
Dennis J. McWherter Jr <dmcwherter@oath.com>
2017-09-15 17:35:12 -0500

    Cleanup.

--- elide-core/src/main/java/com/yahoo/elide/core/PersistentResource.java
+++ elide-core/src/main/java/com/yahoo/elide/core/PersistentResource.java
@@ -814,40 +814,30 @@ public class PersistentResource<T> implements com.yahoo.elide.security.Persisten
         FilterExpression filterExpression;
 
         Class<?> entityType = dictionary.getParameterizedType(getResourceClass(), relation);
-        String typeAlias = (entityType == null) ? null : dictionary.getJsonAliasFor(entityType);
 
-        Set<PersistentResource> resources = new LinkedHashSet<>();
+        Set<PersistentResource> newResources = new LinkedHashSet<>();
 
         /* If this is a bulk edit request and the ID we are fetching for is newly created... */
         if (entityType == null) {
             throw new InvalidAttributeException(relation, type);
         } else {
-            Class<?> idType = dictionary.getIdType(entityType);
-            String idField = dictionary.getIdFieldName(entityType);
-
             if (!ids.isEmpty()) {
-                Map<String, PersistentResource> newResources = requestScope.getNewPersistentResources().stream()
+                String typeAlias = dictionary.getJsonAliasFor(entityType);
+                Map<String, PersistentResource> newResourceMap = requestScope.getNewPersistentResources().stream()
                         .filter(r -> typeAlias.equals(r.getType()))
                         .collect(Collectors.toMap(r -> (String) r.getUUID().orElse(""), r -> r));
+
                 // Fetch our set of new resources that we know about since we can't find them in the datastore
-                resources = new LinkedHashSet<>(ids.stream()
-                        .filter(newResources::containsKey)
-                        .map(newResources::get)
-                        .collect(Collectors.toSet()));
-                FilterExpression idExpression = new FilterPredicate(
-                        new FilterPredicate.PathElement(
-                                entityType,
-                                idType,
-                                idField),
-                        Operator.IN,
-                        ids.stream()
-                                // Filter out new ids from our expression-- these don't exist in the datastore yet
-                                .filter(id ->
-                                        requestScope.getObjectById(dictionary.getJsonAliasFor(entityType), id) == null)
-                                .map(id -> CoerceUtil.coerce(id, idType)).collect(Collectors.toList()));
+                newResources = new LinkedHashSet<>(ids.stream()
+                        .filter(newResourceMap::containsKey)
+                        .map(newResourceMap::get)
+                        .collect(Collectors.toList()));
+
+                FilterExpression idExpression = buildIdFilterExpression(ids, entityType);
+
                 // Combine filters if necessary
                 filterExpression = filter
-                        .map(fe -> (FilterExpression) new AndFilterExpression(idExpression, fe))
+                        .map(fe -> joinFilterExpressions(idExpression, fe))
                         .orElse(idExpression);
             } else {
                 filterExpression = filter.orElse(null);
@@ -857,23 +847,15 @@ public class PersistentResource<T> implements com.yahoo.elide.security.Persisten
         // TODO: Filter on new resources?
         // TODO: Update pagination to subtract the number of new resources created?
 
-        resources.addAll(filter(ReadPermission.class,
-            (Set) getRelationChecked(relation, Optional.ofNullable(filterExpression), sorting, pagination)));
+        Set<PersistentResource> existingResources = filter(ReadPermission.class,
+            (Set) getRelationChecked(relation, Optional.ofNullable(filterExpression), sorting, pagination));
 
         // TODO: Sort again in memory now that two sets are glommed together?
 
-        Set<String> added = new HashSet<>();
-        for (PersistentResource childResource : resources) {
-            for (String id : ids) {
-                if (childResource.matchesId(id)) {
-                    added.add(id);
-                    break;
-                }
-            }
-        }
+        Set<PersistentResource> allResources = Sets.union(newResources, existingResources);
 
         String missedIds = ids.stream()
-                .filter(id -> !added.contains(id))
+                .filter(id -> allResources.stream().noneMatch(p -> p.matchesId(id)))
                 .reduce(new StringBuilder(),
                         (prev, curr) -> (prev.length() > 0) ? prev.append(curr).append(",") : prev.append(curr),
                         StringBuilder::append)
@@ -883,7 +865,42 @@ public class PersistentResource<T> implements com.yahoo.elide.security.Persisten
             throw new InvalidObjectIdentifierException(missedIds, relation);
         }
 
-        return resources;
+        return allResources;
+    }
+
+    /**
+     * Build an id filter expression for a particular entity type.
+     *
+     * @param ids Ids to include in the filter expression
+     * @param entityType Type of entity
+     * @return Filter expression for given ids and type.
+     */
+    private FilterExpression buildIdFilterExpression(List<String> ids, Class entityType) {
+        Class<?> idType = dictionary.getIdType(entityType);
+        String idField = dictionary.getIdFieldName(entityType);
+        String typeAlias = dictionary.getJsonAliasFor(entityType);
+
+        return new FilterPredicate(
+                new FilterPredicate.PathElement(
+                        entityType,
+                        idType,
+                        idField),
+                Operator.IN,
+                ids.stream()
+                        // Filter out new ids from our expression-- these don't exist in the datastore yet
+                        .filter(id -> requestScope.getObjectById(typeAlias, id) == null)
+                        .map(id -> CoerceUtil.coerce(id, idType)).collect(Collectors.toList()));
+    }
+
+    /**
+     * Join two filter expressions with an AND clause.
+     *
+     * @param left Left expression
+     * @param right Right expression
+     * @return Expression representing left AND right.
+     */
+    private FilterExpression joinFilterExpressions(FilterExpression left, FilterExpression right) {
+        return new AndFilterExpression(left, right);
     }
 
     /**
