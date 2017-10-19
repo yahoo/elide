085864aa2bc3ce0c87811d41a8d670c55bcba2d2
suriyasundar <suriyasundar@yahoo-inc.com>
2017-10-19 13:39:08 -0500

    Defering all inline checks for newly created entities

--- elide-core/src/main/java/com/yahoo/elide/security/executors/ActivePermissionExecutor.java
+++ elide-core/src/main/java/com/yahoo/elide/security/executors/ActivePermissionExecutor.java
@@ -123,8 +123,7 @@ public class ActivePermissionExecutor implements PermissionExecutor {
 
         Function<Expression, ExpressionResult> expressionExecutor = (expression) -> {
             // for newly created object in PatchRequest limit to User checks
-            if (((RequestScope) resource.getRequestScope()).isMutatingMultipleEntities()
-                    && requestScope.getNewPersistentResources().contains(resource)) {
+            if (requestScope.getNewPersistentResources().contains(resource)) {
                 return executeUserChecksDeferInline(annotationClass, expression);
             }
             return executeExpressions(expression, annotationClass, Expression.EvaluationMode.INLINE_CHECKS_ONLY);
@@ -212,8 +211,7 @@ public class ActivePermissionExecutor implements PermissionExecutor {
         };
 
         Function<Expression, ExpressionResult> expressionExecutor = (expression) -> {
-            if (((RequestScope) resource.getRequestScope()).isMutatingMultipleEntities()
-                    && requestScope.getNewPersistentResources().contains(resource)) {
+            if (requestScope.getNewPersistentResources().contains(resource)) {
                 return executeUserChecksDeferInline(expressionAnnotation, expression);
             }
             return executeExpressions(expression, expressionAnnotation, Expression.EvaluationMode.INLINE_CHECKS_ONLY);
