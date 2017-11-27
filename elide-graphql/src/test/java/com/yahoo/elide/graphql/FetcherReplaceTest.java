0dd0e753a12c5a0ef625f5d521875f4a4cd1b214
Dennis J. McWherter, Jr <dmcwherter@oath.com>
2017-11-27 14:24:54 -0600

    Add replace tests.

---
+++ elide-graphql/src/test/java/com/yahoo/elide/graphql/FetcherReplaceTest.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2017, Oath Inc.
+ * Licensed under the Apache License, Version 2.0
+ * See LICENSE file in project root for terms.
+ */
+package com.yahoo.elide.graphql;
+
+import org.testng.annotations.Test;
+
+public class FetcherReplaceTest extends PersistentResourceFetcherTest {
+
+    @Test
+    public void testReplaceAttributeRoot() throws Exception {
+        runComparisonTest("replaceRootCollection");
+    }
+
+    @Test
+    public void testReplaceNestedCollection() throws Exception {
+        runComparisonTest("replaceNestedCollection");
+    }
+
+    @Test
+    public void testReplaceEmptyCollections() throws Exception {
+        runComparisonTest("replaceEmptyCollections");
+    }
+
+    // FIXME: Remove stack traces from error handler...
+    @Test(enabled = false)
+    public void testReplaceWithIdsFails() throws Exception {
+        runErrorComparisonTest("replaceWithIdsFails");
+    }
+
+    @Override
+    public void runComparisonTest(String testName) throws Exception {
+        super.runComparisonTest("replace/" + testName);
+    }
+
+    @Override
+    public void runErrorComparisonTest(String testName) throws Exception {
+        super.runErrorComparisonTest("replace/" + testName);
+    }
+}
