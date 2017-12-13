55eb61fdc495426a8bca2b62003a5ed10f185f19
Aaron Klish <aklish@gmail.com>
2017-12-13 10:07:19 -0600

    Updated attribution for MutableGraphQLInputObjectType.java

--- elide-graphql/src/main/java/com/yahoo/elide/graphql/MutableGraphQLInputObjectType.java
+++ elide-graphql/src/main/java/com/yahoo/elide/graphql/MutableGraphQLInputObjectType.java
@@ -18,11 +18,31 @@ import java.util.Map;
 
 import static graphql.Assert.assertNotNull;
 
-
 /**
  * Basically the same class as GraphQLInputObjectType except fields can be added after the
  * object is constructed.  This mutable behavior is useful for constructing input objects from
  * object graphs with cycles.
+ *
+ * Portions of this file are taken from GraphQL-Java which has the following license:
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Andreas Marek and Contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction, including without
+ * limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 public class MutableGraphQLInputObjectType extends GraphQLInputObjectType {
 
