413383b22b0dde9a2dae1ffec92ac1ec7ba47ae9
Dennis J. McWherter, Jr <dmcwherter@oath.com>
2017-12-20 14:40:50 -0600

    Fix exception handling.

---
+++ elide-core/src/main/java/com/yahoo/elide/core/exceptions/InvalidConstraintException.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2017, Yahoo Inc.
+ * Licensed under the Apache License, Version 2.0
+ * See LICENSE file in project root for terms.
+ */
+package com.yahoo.elide.core.exceptions;
+import com.yahoo.elide.core.HttpStatus;
+
+/**
+ * Invalid constraint exception. Message is exactly what is provided in the constructor.
+ *
+ * {@link com.yahoo.elide.core.HttpStatus#SC_BAD_REQUEST invalid}
+ */
+public class InvalidConstraintException extends HttpStatusException {
+    private static final long serialVersionUID = 1L;
+
+    public InvalidConstraintException(String message) {
+        super(HttpStatus.SC_BAD_REQUEST, message);
+    }
+
+    @Override
+    public String toString() {
+        String message = getMessage();
+
+        if (message == null) {
+            return getClass().getSimpleName();
+        }
+
+        return message;
+    }
+}
