7c74de4e92c548b1e338b6ad349d1b10a248db2c
Aaron Klish <aklish@gmail.com>
2018-04-11 15:56:39 -0500

    A bit of polishing and tests

--- elide-core/src/test/java/example/Author.java
+++ elide-core/src/test/java/example/Author.java
@@ -39,7 +39,7 @@ public class Author {
         FREELANCE
     }
 
-    private long id;
+    private Long id;
     private String name;
     private Collection<Book> books = new ArrayList<>();
     private AuthorType type;
@@ -49,11 +49,11 @@ public class Author {
     private Address homeAddress;
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
