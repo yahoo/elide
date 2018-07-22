85dd0dd70bbe8eba7afc3f35ba464bf82ad12c35
kubo <jsodpad@gmail.com>
2018-07-22 14:36:58 +0200

    Fix #677 Nullpointer when using HibernateEntityManagerStore

--- elide-datastore/elide-datastore-hibernate5/src/main/java/com/yahoo/elide/datastores/hibernate5/HibernateEntityManagerStore.java
+++ elide-datastore/elide-datastore-hibernate5/src/main/java/com/yahoo/elide/datastores/hibernate5/HibernateEntityManagerStore.java
@@ -6,10 +6,14 @@
 package com.yahoo.elide.datastores.hibernate5;
 
 import com.yahoo.elide.core.DataStoreTransaction;
+import com.yahoo.elide.core.EntityDictionary;
+
 import org.hibernate.ScrollMode;
 import org.hibernate.Session;
 import org.hibernate.jpa.HibernateEntityManager;
+
 import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
 
 /**
  * Hibernate5 store supporting the EntityManager.
@@ -54,5 +58,13 @@ public class HibernateEntityManagerStore extends AbstractHibernateStore {
         session.beginTransaction();
         session.clear();
         return transactionSupplier.get(session, isScrollEnabled, scrollMode);
+    }
+
+    @Override
+    public void populateEntityDictionary(EntityDictionary dictionary) {
+        /* bind all entities */
+        for (EntityType type : entityManager.getEntityManagerFactory().getMetamodel().getEntities()) {
+            bindEntity(dictionary, type);
+        }
     }
 }
