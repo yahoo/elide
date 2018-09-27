ecdf9693dc25655cb8143f00165d7280d8916c1e
Christian Schmidt <c-schmidt@gmx.eu>
2018-09-27 23:27:55 +0200

    Add tests for field level scanning

---
+++ elide-integration-tests/src/test/java/com/yahoo/elide/fieldLevelTest/FieldLevelParentClassIdIT.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2017, Yahoo Inc.
+ * Licensed under the Apache License, Version 2.0
+ * See LICENSE file in project root for terms.
+ */
+package com.yahoo.elide.fieldLevelTest;
+
+import com.yahoo.elide.initialization.AbstractIntegrationTestInitializer;
+import com.yahoo.elide.initialization.AssignedIdLongIntegrationTestApplicationResourceConfig;
+import com.yahoo.elide.utils.JsonParser;
+import org.apache.http.HttpStatus;
+import org.testng.annotations.Test;
+
+import static com.jayway.restassured.RestAssured.given;
+
+public class FieldLevelParentClassIdIT extends AbstractIntegrationTestInitializer {
+    private final JsonParser jsonParser = new JsonParser();
+
+    private static final String JSONAPI_CONTENT_TYPE = "application/vnd.api+json";
+
+    public FieldLevelParentClassIdIT() {
+        super(AssignedIdLongIntegrationTestApplicationResourceConfig.class);
+    }
+
+    @Test(priority = 0)
+    public void testResponseCodeOnUpdate() {
+        String request = jsonParser.getJson("/FieldLevelIT/createFieldLevelChildEntity.req.json");
+        String expected = jsonParser.getJson("/FieldLevelIT/createFieldLevelChildEntity.resp.json");
+
+        String actual = given()
+                .contentType(JSONAPI_CONTENT_TYPE)
+                .accept(JSONAPI_CONTENT_TYPE)
+                .body(request)
+                .post("/fieldLevelChild")
+                .then()
+                .statusCode(HttpStatus.SC_CREATED)
+                .extract().body().asString();
+        assertEqualDocuments(actual, expected);
+
+        request = jsonParser.getJson("/FieldLevelIT/updateFieldLevelChildEntity.req.json");
+        expected = jsonParser.getJson("/FieldLevelIT/updateFieldLevelChildEntity.resp.json");
+
+        actual = given()
+                .contentType(JSONAPI_CONTENT_TYPE)
+                .accept(JSONAPI_CONTENT_TYPE)
+                .body(request)
+                .patch("/fieldLevelChild/1")
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .extract().body().asString();
+        assertEqualDocuments(actual, expected);
+    }
+}
---
+++ elide-integration-tests/src/test/java/example/FieldLevelChildEntity.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2018, Yahoo Inc.
+ * Licensed under the Apache License, Version 2.0
+ * See LICENSE file in project root for terms.
+ */
+package example;
+
+import com.yahoo.elide.annotation.Include;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "child_field_level")
+@Include(rootLevel = true)
+public class FieldLevelChildEntity extends FieldLevelParentSuperclass {
+    private String childField;
+}
