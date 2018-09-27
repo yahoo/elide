fa17d5aa6f1bae5fd14c571e06a2ef225f8b6747
Christian Schmidt <c-schmidt@gmx.eu>
2018-09-27 23:27:55 +0200

    Add tests for field level scanning

---
+++ elide-core/src/test/java/com/yahoo/elide/core/EntityBindingTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2018, Yahoo Inc.
+ * Licensed under the Apache License, Version 2.0
+ * See LICENSE file in project root for terms.
+ */
+package com.yahoo.elide.core;
+
+import org.mockito.Mock;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import javax.persistence.Id;
+import java.lang.reflect.AccessibleObject;
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
+
+public class EntityBindingTest {
+    private EntityBinding entityBinding;
+
+    @Mock
+    EntityDictionary entityDictionary;
+
+    @BeforeTest
+    public void init() {
+        entityBinding = new EntityBinding(entityDictionary, ChildClass.class, "child", "childBinding");
+    }
+
+    @Test
+    public void testGetAllFields() throws Exception {
+        List<AccessibleObject> allFields = entityBinding.getAllFields();
+
+        assertEquals(allFields.size(), 2);
+        assertEquals(allFields.get(0), ChildClass.class.getDeclaredField("childField"));
+        assertEquals(allFields.get(1), ParentClass.class.getDeclaredField("parentField"));
+    }
+
+    private class ParentClass {
+        @Id
+        String parentField;
+
+        public void ignoredMethod() { }
+    }
+
+    private class ChildClass extends ParentClass {
+        String childField;
+    }
+}
