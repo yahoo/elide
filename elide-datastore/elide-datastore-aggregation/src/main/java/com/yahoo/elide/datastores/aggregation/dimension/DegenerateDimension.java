19fb6079c6cb6cbff157a89fc117e83616dd156b
Jack (정환) <QubitPi@users.noreply.github.com>
2019-07-12 17:29:49 -0700

    AggregationDataStore: Schema (#846)
    * AggregationDataStore: Static Attribute Aggregation
    
    * Address comments
    
    * Implement TimeDimension and all its supporting components
    
    * refactor
    
    * Address comments from @aklish
    
    * Address comments from @aklish && Tests & Javadoc
    
    * Address comments from @aklish
    
    * Address comments from @aklish and @hellohanchen
    
    * Address comments from Aaron
    
    * ToMany is not supported
    
    * Address comments from Aaron

---
+++ elide-datastore/elide-datastore-aggregation/src/main/java/com/yahoo/elide/datastores/aggregation/dimension/DegenerateDimension.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2019, Yahoo Inc.
+ * Licensed under the Apache License, Version 2.0
+ * See LICENSE file in project root for terms.
+ */
+package com.yahoo.elide.datastores.aggregation.dimension;
+
+import com.yahoo.elide.core.EntityDictionary;
+import com.yahoo.elide.datastores.aggregation.annotation.CardinalitySize;
+import com.yahoo.elide.datastores.aggregation.annotation.Meta;
+
+import lombok.Getter;
+
+import java.util.Objects;
+import java.util.StringJoiner;
+
+/**
+ * A {@link Dimension} backed by a table column.
+ * <p>
+ * {@link DegenerateDimension} is thread-safe and can be accessed by multiple threads.
+ */
+public class DegenerateDimension extends EntityDimension {
+
+    /**
+     * Returns the nature of a SQL column that is backing a specified dimension field in an entity.
+     *
+     * @param dimensionField  The provided dimension field
+     * @param cls  The entity having the {@code dimensionField}
+     * @param entityDictionary  Object that helps to find the type info about entity field
+     *
+     * @return a {@link ColumnType}, such as PK or a regular field
+     */
+    public static ColumnType parseColumnType(String dimensionField, Class<?> cls, EntityDictionary entityDictionary) {
+        if (entityDictionary.getIdFieldName(cls).equals(dimensionField)) {
+            return ColumnType.PRIMARY_KEY;
+        } else {
+            return ColumnType.FIELD;
+        }
+    }
+
+    private static final long serialVersionUID = 370506626352850507L;
+
+    @Getter
+    ColumnType columnType;
+
+    /**
+     * Constructor.
+     *
+     * @param dimensionField  The entity field or relation that this {@link Dimension} represents
+     * @param annotation  Provides static meta data about this {@link Dimension}
+     * @param fieldType  The Java type for this entity field or relation
+     * @param cardinality  The estimated cardinality of this {@link Dimension} in SQL table
+     * @param friendlyName  A human-readable name representing this {@link Dimension}
+     * @param columnType  The type of the SQL column mapping this {@link Dimension}
+     *
+     * @throws NullPointerException any argument, except for {@code annotation}, is {@code null}
+     */
+    public DegenerateDimension(
+            String dimensionField,
+            Meta annotation,
+            Class<?> fieldType,
+            CardinalitySize cardinality,
+            String friendlyName,
+            ColumnType columnType
+    ) {
+        super(
+                dimensionField,
+                annotation,
+                fieldType,
+                DimensionType.DEGENERATE,
+                Objects.requireNonNull(cardinality, "cardinality"),
+                Objects.requireNonNull(friendlyName, "friendlyName")
+        );
+        this.columnType = Objects.requireNonNull(columnType, "columnType");
+    }
+
+    @Override
+    public boolean equals(final Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (other == null || getClass() != other.getClass()) {
+            return false;
+        }
+        if (!super.equals(other)) { return false; }
+
+        final DegenerateDimension that = (DegenerateDimension) other;
+        return getColumnType().equals(that.getColumnType());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), getColumnType());
+    }
+
+    /**
+     * Returns the string representation of this {@link Dimension}.
+     * <p>
+     * The string consists of values of all fields in the format
+     * "DegenerateDimension[columnType=XXX, name='XXX', longName='XXX', description='XXX', dimensionType=XXX,
+     * dataType=XXX, cardinality=XXX, friendlyName=XXX]", where values can be programmatically fetched via getters.
+     * <p>
+     * {@code dataType} are printed in its {@link Class#getSimpleName() simple name}.
+     * <p>
+     * Note that there is a single space separating each value pair.
+     *
+     * @return serialized {@link DegenerateDimension}
+     */
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", DegenerateDimension.class.getSimpleName() + "[", "]")
+                .add("columnType=" + getColumnType())
+                .add("name='" + getName() + "'")
+                .add("longName='" + getLongName() + "'")
+                .add("description='" + getDescription() + "'")
+                .add("dimensionType=" + getDimensionType())
+                .add("dataType=" + getDataType().getSimpleName())
+                .add("cardinality=" + getCardinality())
+                .add("friendlyName='" + getFriendlyName() + "'")
+                .toString();
+    }
+}
---
+++ elide-datastore/elide-datastore-aggregation/src/test/java/com/yahoo/elide/datastores/aggregation/metric/AggregatedMetricTest.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2019, Yahoo Inc.
+ * Licensed under the Apache License, Version 2.0
+ * See LICENSE file in project root for terms.
+ */
+package com.yahoo.elide.datastores.aggregation.metric;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class AggregatedMetricTest {
+
+    private static final Metric SIMPLE_METRIC_1 = new AggregatedMetric(
+            "highScore",
+            null,
+            long.class,
+            Collections.singletonList(Max.class)
+    );
+
+    private static final Metric SIMPLE_METRIC_2 = new AggregatedMetric(
+            "timeSpentPerGame",
+            null,
+            Float.class,
+            Collections.singletonList(Max.class)
+    );
+
+    @Test
+    public void testMetricAsCollectionElement() {
+        Assert.assertEquals(SIMPLE_METRIC_1, SIMPLE_METRIC_1);
+        Assert.assertEquals(SIMPLE_METRIC_2, SIMPLE_METRIC_2);
+        Assert.assertNotEquals(SIMPLE_METRIC_1, SIMPLE_METRIC_2);
+        Assert.assertNotEquals(SIMPLE_METRIC_1.hashCode(), SIMPLE_METRIC_2.hashCode());
+
+        // different metrics should be separate elements in Set
+        Set<Metric> set = new HashSet<>();
+        set.add(SIMPLE_METRIC_1);
+
+        Assert.assertEquals(set.size(), 1);
+
+        // a separate same object doesn't increase collection size
+        Metric sameMetric = new AggregatedMetric(
+                "highScore",
+                null,
+                long.class,
+                Collections.singletonList(Max.class)
+        );
+        Assert.assertEquals(sameMetric, SIMPLE_METRIC_1);
+        set.add(sameMetric);
+        Assert.assertEquals(set.size(), 1);
+
+        set.add(SIMPLE_METRIC_1);
+        Assert.assertEquals(set.size(), 1);
+
+        set.add(SIMPLE_METRIC_2);
+        Assert.assertEquals(set.size(), 2);
+    }
+
+    @Test
+    public void testToString() {
+        // simple metric
+        Assert.assertEquals(
+                SIMPLE_METRIC_1.toString(),
+                "AggregatedMetric[name='highScore', longName='highScore', description='highScore', dataType=long, aggregations=Max]"
+        );
+
+        // computed metric
+        Assert.assertEquals(
+                SIMPLE_METRIC_2.toString(),
+                "AggregatedMetric[name='timeSpentPerGame', longName='timeSpentPerGame', description='timeSpentPerGame', dataType=class java.lang.Float, aggregations=Max]"
+        );
+    }
+}
