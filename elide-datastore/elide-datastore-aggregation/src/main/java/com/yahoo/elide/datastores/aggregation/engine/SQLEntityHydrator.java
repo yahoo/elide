dae947e326f0a3f4e43383063bdb3cae4d6e8e59
Jiaqi Liu <2257440489@qq.com>
2019-07-18 17:55:37 -0700

    Self-review

--- elide-datastore/elide-datastore-aggregation/src/main/java/com/yahoo/elide/datastores/aggregation/engine/SQLEntityHydrator.java
+++ elide-datastore/elide-datastore-aggregation/src/main/java/com/yahoo/elide/datastores/aggregation/engine/SQLEntityHydrator.java
@@ -33,24 +33,6 @@ public class SQLEntityHydrator extends AbstractEntityHydrator {
         this.entityManager = entityManager;
     }
 
-    /**
-     * Returns a list of relationship objects whose ID matches a specified list of ID's
-     * <p>
-     * Note the relationship cannot be toMany. Regardless of ID duplicates, this method returns the list of relationship
-     * objects in the same size as the specified list of ID's, i.e. duplicate ID mapps to the same object in the
-     * returned list. For example:
-     * <p>
-     * if {@code entityClass = Country.java}, {@code joinField = country}, and {@code joinFieldIds = [840, 840, 344]},
-     * then this method returns {@code Country(id:840), Country(id:840), Country(id:344)}.
-     * <p>
-     * If the ID list is empty or no matching objects are found, this method returns {@link Collections#emptyList()}.
-     *
-     * @param entityClass  The type of relationship
-     * @param joinField  The relationship field name
-     * @param joinFieldIds  The specified list of join ID's against the relationshiop
-     *
-     * @return a list of hydrating values
-     */
     @Override
     public Map<Object, Object> getRelationshipValues(Class<?> entityClass, String joinField, List<Object> joinFieldIds) {
         if (joinFieldIds.isEmpty()) {
@@ -62,8 +44,7 @@ public class SQLEntityHydrator extends AbstractEntityHydrator {
         List<Object> loaded = getEntityManager()
                 .createQuery(
                         String.format(
-                                "SELECT %s FROM %s WHERE %s IN :idList",
-                                entityClass.getCanonicalName(),
+                                "SELECT e FROM %s e WHERE %s IN (:idList)",
                                 entityClass.getCanonicalName(),
                                 getEntityDictionary().getIdFieldName(entityClass)
                         )
