39924145a7299481adc75ec0f6fe37ab614b020e
Aaron Klish <klish@verizonmedia.com>
2019-07-22 10:51:52 -0500

    IncludedProcessorTest refactored

--- elide-core/src/test/java/com/yahoo/elide/core/TestRequestScope.java
+++ elide-core/src/test/java/com/yahoo/elide/core/TestRequestScope.java
@@ -6,46 +6,55 @@
 
 package com.yahoo.elide.core;
 
-import com.yahoo.elide.ElideSettings;
 import com.yahoo.elide.ElideSettingsBuilder;
 import com.yahoo.elide.jsonapi.models.JsonApiDocument;
 import com.yahoo.elide.request.EntityProjection;
+import com.yahoo.elide.request.EntityProjection.EntityProjectionBuilder;
 import com.yahoo.elide.security.User;
 
-import javax.ws.rs.core.MultivaluedMap;
+import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Utility subclass that helps construct RequestScope objects for testing.
+ */
 public class TestRequestScope extends RequestScope {
-    public TestRequestScope(String path,
-                        JsonApiDocument jsonApiDocument,
-                        DataStoreTransaction transaction,
+    public TestRequestScope(DataStoreTransaction transaction,
                         User user,
-                        MultivaluedMap<String, String> queryParams,
                         EntityDictionary dictionary,
-                        Class<?> entityClass) {
-        super(path, jsonApiDocument, transaction, user, queryParams, new ElideSettingsBuilder(null)
+                        Class<?> entityClass,
+                        int nestLevel) {
+        super(null, new JsonApiDocument(), transaction, user, null,
+                new ElideSettingsBuilder(null)
                 .withEntityDictionary(dictionary)
                 .build());
 
-        setEntityProjection(EntityProjection.builder()
-                .type(entityClass)
-                .dictionary(dictionary)
-                .relationships(
-                        dictionary.getRelationships(entityClass).stream()
-                                .collect(Collectors.toMap(
-                                        (name) -> {
-                                            return name;
-                                        },
-                                        (name) -> {
-                                            return EntityProjection.builder()
-                                                    .dictionary(dictionary)
-                                                    .type(dictionary.getType(entityClass, name))
-                                                    .build();
-                                        }
-                                ))
-                )
-                .build());
+        /* Create an entity projection from a top level object and all of its relationships */
+        setEntityProjection(buildEntityProjection(entityClass, nestLevel));
     }
 
+    EntityProjection buildEntityProjection(Class<?> entityClass, int nestLevel) {
 
+        /* Create an entity projection from a top level object and all of its relationships */
+        EntityProjectionBuilder builder = EntityProjection.builder()
+                .type(entityClass)
+                .dictionary(dictionary);
+
+        List<String> relationships =  dictionary.getRelationships(entityClass);
+        if (nestLevel > 0  && relationships != null && !relationships.isEmpty()) {
+            builder.relationships(
+                dictionary.getRelationships(entityClass).stream()
+                    .collect(Collectors.toMap(
+                        (name) -> {
+                            return name;
+                        },
+                        (name) -> {
+                            return buildEntityProjection(dictionary.getParameterizedType(entityClass, name), nestLevel - 1);
+                        }
+                    ))
+            );
+        }
+
+        return builder.build();
+    }
 }
