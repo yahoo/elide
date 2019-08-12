8c27c1fabc90723a594c68ffcd950f3dd7506b0e
hchen04 <han.chen@oath.com>
2019-08-12 14:11:40 -0500

    Elide-Testing Refactor audit integration test

--- elide-integration-tests/src/test/java/com/yahoo/elide/auditTests/AuditIT.java
+++ elide-integration-tests/src/test/java/com/yahoo/elide/auditTests/AuditIT.java
@@ -6,30 +6,51 @@
 package com.yahoo.elide.auditTests;
 
 import static com.jayway.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.yahoo.elide.audit.InMemoryLogger;
-import com.yahoo.elide.initialization.AbstractIntegrationTestInitializer;
 import com.yahoo.elide.initialization.AuditIntegrationTestApplicationResourceConfig;
+import com.yahoo.elide.initialization.IntegrationTest;
+import com.yahoo.elide.resources.JsonApiEndpoint;
 import com.yahoo.elide.utils.JsonParser;
 
 import org.apache.http.HttpStatus;
-import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 /**
  * Integration tests for audit functionality.
  */
-public class AuditIT extends AbstractIntegrationTestInitializer {
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class AuditIT extends IntegrationTest {
     private final InMemoryLogger logger = AuditIntegrationTestApplicationResourceConfig.LOGGER;
     private final JsonParser jsonParser = new JsonParser();
 
     private static final String JSONAPI_CONTENT_TYPE = "application/vnd.api+json";
 
     public AuditIT() {
-        super(AuditIntegrationTestApplicationResourceConfig.class);
+        super(AuditIntegrationTestApplicationResourceConfig.class, JsonApiEndpoint.class.getPackage().getName());
     }
 
-    @Test(priority = 0)
+    @AfterEach
+    @Override
+    public void afterEach() {
+        // Don't clean up the database between tests
+    }
+
+    @AfterAll
+    public void clear() {
+        super.afterEach(); // clean up the database
+        super.afterAll(); // stop the server
+    }
+
+    @Test
+    @Order(1)
     public void testAuditOnCreate() throws Exception {
         String request = jsonParser.getJson("/AuditIT/createAuditEntity.req.json");
         String expected = jsonParser.getJson("/AuditIT/createAuditEntity.resp.json");
@@ -44,12 +65,13 @@ public class AuditIT extends AbstractIntegrationTestInitializer {
                 .extract().body().asString();
 
         assertEqualDocuments(actual, expected);
-        Assert.assertTrue(logger.logMessages.contains("old: null\n"
+        assertTrue(logger.logMessages.contains("old: null\n"
                 + "new: Value: test abc relationship: null"));
-        Assert.assertTrue(logger.logMessages.contains("Created with value: test abc"));
+        assertTrue(logger.logMessages.contains("Created with value: test abc"));
     }
 
-    @Test(priority = 1)
+    @Test
+    @Order(2)
     public void testAuditOnUpdate() {
         String request = jsonParser.getJson("/AuditIT/createAuditEntity2.req.json");
         String expected = jsonParser.getJson("/AuditIT/createAuditEntity2.resp.json");
@@ -75,11 +97,12 @@ public class AuditIT extends AbstractIntegrationTestInitializer {
                 .then()
                 .statusCode(HttpStatus.SC_NO_CONTENT);
 
-        Assert.assertTrue(logger.logMessages.contains("Updated relationship (for id: 1): 2"));
-        Assert.assertTrue(logger.logMessages.contains("Updated value (for id: 1): updated value"));
+        assertTrue(logger.logMessages.contains("Updated relationship (for id: 1): 2"));
+        assertTrue(logger.logMessages.contains("Updated value (for id: 1): updated value"));
     }
 
-    @Test(priority = 2)
+    @Test
+    @Order(3)
     public void testAuditWithDuplicateLineageEntry() {
         String request = jsonParser.getJson("/AuditIT/updateAuditEntityLineageDup.req.json");
 
@@ -91,14 +114,15 @@ public class AuditIT extends AbstractIntegrationTestInitializer {
                 .then()
                 .statusCode(HttpStatus.SC_NO_CONTENT);
 
-        Assert.assertTrue(logger.logMessages.contains("Updated value (for id: 1): update id 1 through id 2"));
+        assertTrue(logger.logMessages.contains("Updated value (for id: 1): update id 1 through id 2"));
     }
 
-    @Test(priority = 3)
+    @Test
+    @Order(4)
     public void testAuditUpdateOnInverseCollection() {
         String request = jsonParser.getJson("/AuditIT/createAuditEntityInverse.req.json");
 
-        Assert.assertFalse(logger.logMessages.contains("Inverse entities: [Value: update id 1 through id 2 relationship: 2]"));
+        assertFalse(logger.logMessages.contains("Inverse entities: [Value: update id 1 through id 2 relationship: 2]"));
 
         given()
                 .contentType(JSONAPI_CONTENT_TYPE)
@@ -108,8 +132,8 @@ public class AuditIT extends AbstractIntegrationTestInitializer {
                 .then()
                 .statusCode(HttpStatus.SC_CREATED);
 
-        Assert.assertTrue(logger.logMessages.contains("Entity with id 1 now has inverse list [AuditEntityInverse{id=1, entities=[Value: update id 1 through id 2 relationship: 2]}]"));
-        Assert.assertTrue(logger.logMessages.contains("Inverse entities: [Value: update id 1 through id 2 relationship: 2]"));
+        assertTrue(logger.logMessages.contains("Entity with id 1 now has inverse list [AuditEntityInverse{id=1, entities=[Value: update id 1 through id 2 relationship: 2]}]"));
+        assertTrue(logger.logMessages.contains("Inverse entities: [Value: update id 1 through id 2 relationship: 2]"));
 
         // This message may have been added on create. Remove it so we don't get a false positive.
         // NOTE: Our internal audit loggers handle this behavior by ignoring update messages associated with
@@ -125,7 +149,7 @@ public class AuditIT extends AbstractIntegrationTestInitializer {
                 .then()
                 .statusCode(HttpStatus.SC_NO_CONTENT);
 
-        Assert.assertTrue(logger.logMessages.contains("Entity with id 1 now has inverse list []"));
-        Assert.assertTrue(logger.logMessages.contains("Inverse entities: []"));
+        assertTrue(logger.logMessages.contains("Entity with id 1 now has inverse list []"));
+        assertTrue(logger.logMessages.contains("Inverse entities: []"));
     }
 }
