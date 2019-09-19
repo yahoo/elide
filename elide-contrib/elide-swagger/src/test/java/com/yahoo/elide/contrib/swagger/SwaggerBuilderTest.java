88d5bb53dda1d5e2d651183e771f56ae23cf5e42
Aaron Klish <klish@verizonmedia.com>
2019-09-19 09:54:32 -0500

    Removed unused imports:

--- elide-contrib/elide-swagger/src/test/java/com/yahoo/elide/contrib/swagger/SwaggerBuilderTest.java
+++ elide-contrib/elide-swagger/src/test/java/com/yahoo/elide/contrib/swagger/SwaggerBuilderTest.java
@@ -46,7 +46,6 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 
 public class SwaggerBuilderTest {
