45a0cfdefca1f05de99d1c3437603facb66624cd
Aaron Klish <klish@verizonmedia.com>
2019-11-14 11:07:36 -0600

    Fixed git issue where submodule was lost.  Added back all the code

---
+++ elide-spring/README.md
@@ -0,0 +1,7 @@
+# Elide Spring Artifacts
+
+Elide comes bundled with two Spring artifacts:
+
+1. [elide-spring-boot-autoconfigure](https://github.com/yahoo/elide/blob/master/elide-spring/elide-spring-boot-autoconfigure/README.md) - Provides controllers and configuration for using Elide with Spring Boot.
+2. elide-spring-boot-starter - **TBD** - Provides a Spring Boot dependency starter for an opinionated setup.
+
---
+++ elide-spring/elide-spring-boot-autoconfigure/src/main/java/com/yahoo/elide/spring/config/ElideConfigProperties.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2019, Yahoo Inc.
+ * Licensed under the Apache License, Version 2.0
+ * See LICENSE file in project root for terms.
+ */
+package com.yahoo.elide.spring.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+
+/**
+ * Configuration settings for Elide.
+ */
+@Data
+@ConfigurationProperties(prefix = "elide")
+public class ElideConfigProperties {
+
+    /**
+     * Settings for the JSON-API controller.
+     */
+    private ControllerProperties jsonApi;
+
+    /**
+     * Settings for the GraphQL controller.
+     */
+    private ControllerProperties graphql;
+
+    /**
+     * Settings for the Swagger document controller.
+     */
+    private SwaggerControllerProperties swagger;
+
+    /**
+     * Root package name of the Elide data models.
+     */
+    @NotBlank
+    private String modelPackage;
+
+    /**
+     * Default pagination size for collections if the client doesn't paginate.
+     */
+    private int pageSize = 500;
+
+    /**
+     * The maximum pagination size a client can request.
+     */
+    private int maxPageSize = 10000;
+}
