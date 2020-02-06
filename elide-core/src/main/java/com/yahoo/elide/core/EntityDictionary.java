1faa737c5c97695945b5b71687092d58870b4c01
Aaron Klish <aklish@gmail.com>
2020-02-06 17:11:05 -0600

    Update elide-core/src/main/java/com/yahoo/elide/core/EntityDictionary.java
    Co-Authored-By: Jon Kilroy <jkilroy@gmail.com>

--- elide-core/src/main/java/com/yahoo/elide/core/EntityDictionary.java
+++ elide-core/src/main/java/com/yahoo/elide/core/EntityDictionary.java
@@ -833,7 +833,7 @@ public class EntityDictionary {
     public boolean isTransferable(Class<?> entityClass) {
         NonTransferable nonTransferable = getAnnotation(entityClass, NonTransferable.class);
 
-        return (nonTransferable == null || ! nonTransferable.enabled());
+        return (nonTransferable == null || !nonTransferable.enabled());
     }
 
     /**
