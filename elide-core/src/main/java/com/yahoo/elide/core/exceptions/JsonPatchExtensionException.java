d397f9a5259323d6a3a71397c2e932b4d668cd74
Aaron Klish <klish@verizonmedia.com>
2020-02-24 12:52:34 -0600

    Fixed JSON-API Patch Response

--- elide-core/src/main/java/com/yahoo/elide/core/exceptions/JsonPatchExtensionException.java
+++ elide-core/src/main/java/com/yahoo/elide/core/exceptions/JsonPatchExtensionException.java
@@ -5,40 +5,28 @@
  */
 package com.yahoo.elide.core.exceptions;
 
-import com.yahoo.elide.core.ErrorObjects;
-
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import org.apache.commons.lang3.tuple.Pair;
-import org.owasp.encoder.Encode;
 
 /**
  * Exception describing error caused from Json Patch Extension request.
  */
-public class JsonPatchExtensionException extends CustomErrorException {
+public class JsonPatchExtensionException extends HttpStatusException {
     private final Pair<Integer, JsonNode> response;
 
     public JsonPatchExtensionException(int status, final JsonNode errorNode) {
-        super(status, "", encodeResponse(errorNode));
+        super(status, "");
         response = Pair.of(status, errorNode);
     }
 
-    private static ErrorObjects encodeResponse(JsonNode response) {
-        ErrorObjects.ErrorObjectsBuilder errorObjectsBuilder = ErrorObjects.builder();
-        // response is final, so construct a new response with encoded values
-        ArrayNode errors = (ArrayNode) response.get("errors");
-        for (JsonNode node : errors) {
+    @Override
+    public Pair<Integer, JsonNode> getErrorResponse() {
+        return response;
+    }
 
-            ObjectNode objectNode = (ObjectNode) node;
-            errorObjectsBuilder.addError();
-            errorObjectsBuilder = errorObjectsBuilder.withDetail(Encode.forHtml(objectNode.get("detail").asText()));
-
-            if (objectNode.get("status") != null) {
-                errorObjectsBuilder = errorObjectsBuilder.withStatus(objectNode.get("status").asText());
-            }
-        }
-        return errorObjectsBuilder.build();
+    @Override
+    public Pair<Integer, JsonNode> getVerboseErrorResponse() {
+        return response;
     }
 }
