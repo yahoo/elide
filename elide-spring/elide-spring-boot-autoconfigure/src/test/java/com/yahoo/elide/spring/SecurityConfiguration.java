69f64f25b0d65dbb9b7fa00ea229987c961ead54
Aaron Klish <klish@verizonmedia.com>
2020-02-28 11:15:45 -0600

    Adding checkstyle comments for classes

--- elide-spring/elide-spring-boot-autoconfigure/src/test/java/com/yahoo/elide/spring/SecurityConfiguration.java
+++ elide-spring/elide-spring-boot-autoconfigure/src/test/java/com/yahoo/elide/spring/SecurityConfiguration.java
@@ -11,6 +11,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+/**
+ * Disables security for testing.
+ */
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
