a802df13bab343a3c7ae2c1054dd2b5cfd8851f4
wcekan <wcekan@verizonmedia.com>
2020-03-27 15:11:35 -0500

    no filter

--- elide-core/src/main/java/com/yahoo/elide/core/VerifyFieldAccessFilterExpressionVisitor.java
+++ elide-core/src/main/java/com/yahoo/elide/core/VerifyFieldAccessFilterExpressionVisitor.java
@@ -70,8 +70,8 @@ public class VerifyFieldAccessFilterExpressionVisitor implements FilterExpressio
                 .getRelationshipType(entity.getClass(), fieldName) == RelationshipType.NONE) {
             return Stream.empty();
         }
-        Optional<FilterExpression> filterExpression = requestScope.getExpressionForRelation(resource, fieldName);
-        return resource.getRelationChecked(fieldName, filterExpression, Optional.empty(), Optional.empty()).stream();
+        // use no filter to allow the read directly from loaded resource
+        return resource.getRelationChecked(fieldName, Optional.empty(), Optional.empty(), Optional.empty()).stream();
     }
 
     @Override
