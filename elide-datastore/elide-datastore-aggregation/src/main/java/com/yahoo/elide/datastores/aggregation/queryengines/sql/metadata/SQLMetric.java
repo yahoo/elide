0e7fdf2bc537e88b667e675137cd9bc3a61dbe04
Han Chen <hellohanchen@gmail.com>
2020-04-15 11:42:38 -0500

    ColumnProjection Refactor (#1239)
    * unify projections
    
    * remove getFunction()
    
    * add table into query template
    
    Co-authored-by: hchen04 <han.chen@oath.com>

--- elide-datastore/elide-datastore-aggregation/src/main/java/com/yahoo/elide/datastores/aggregation/queryengines/sql/metadata/SQLMetric.java
+++ elide-datastore/elide-datastore-aggregation/src/main/java/com/yahoo/elide/datastores/aggregation/queryengines/sql/metadata/SQLMetric.java
@@ -9,16 +9,23 @@ import com.yahoo.elide.core.EntityDictionary;
 import com.yahoo.elide.datastores.aggregation.metadata.models.FunctionArgument;
 import com.yahoo.elide.datastores.aggregation.metadata.models.Metric;
 import com.yahoo.elide.datastores.aggregation.metadata.models.MetricFunction;
-import com.yahoo.elide.datastores.aggregation.metadata.models.Table;
+import com.yahoo.elide.datastores.aggregation.query.ColumnProjection;
+import com.yahoo.elide.datastores.aggregation.query.MetricProjection;
+import com.yahoo.elide.datastores.aggregation.query.TimeDimensionProjection;
 import com.yahoo.elide.datastores.aggregation.queryengines.sql.metric.SQLMetricFunction;
+import com.yahoo.elide.datastores.aggregation.queryengines.sql.query.SQLQueryTemplate;
+import com.yahoo.elide.request.Argument;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 /**
  * SQLMetric would contain {@link SQLMetricFunction} instead of {@link MetricFunction}.
  */
 public class SQLMetric extends Metric {
-    public SQLMetric(Table table, String fieldName, EntityDictionary dictionary) {
+    public SQLMetric(SQLTable table, String fieldName, EntityDictionary dictionary) {
         super(table, fieldName, dictionary);
     }
 
@@ -29,5 +36,44 @@ public class SQLMetric extends Metric {
                                                      String expression,
                                                      Set<FunctionArgument> arguments) {
         return new SQLMetricFunction(id, longName, description, expression, arguments);
+    }
+
+    /**
+     * Construct a sql query template for a physical table with provided information.
+     * Table name would be filled in when convert the template into real query.
+     *
+     * @param arguments arguments provided in the request
+     * @param alias result alias
+     * @param dimensions groupBy dimensions
+     * @param timeDimension aggregated time dimension
+     * @return <code>SELECT function(arguments, fields) AS alias GROUP BY dimensions, timeDimension </code>
+     */
+    public SQLQueryTemplate resolve(Map<String, Argument> arguments,
+                                    String alias,
+                                    Set<ColumnProjection> dimensions,
+                                    TimeDimensionProjection timeDimension) {
+        MetricProjection projection = ColumnProjection.toMetricProjection(this, alias, arguments);
+
+        return new SQLQueryTemplate() {
+            @Override
+            public SQLTable getTable() {
+                return (SQLTable) projection.getColumn().getTable();
+            }
+
+            @Override
+            public List<MetricProjection> getMetrics() {
+                return Collections.singletonList(projection);
+            }
+
+            @Override
+            public Set<ColumnProjection> getNonTimeDimensions() {
+                return dimensions;
+            }
+
+            @Override
+            public TimeDimensionProjection getTimeDimension() {
+                return timeDimension;
+            }
+        };
     }
 }
--- elide-datastore/elide-datastore-aggregation/src/main/java/com/yahoo/elide/datastores/aggregation/queryengines/sql/metric/SQLMetricFunction.java
+++ elide-datastore/elide-datastore-aggregation/src/main/java/com/yahoo/elide/datastores/aggregation/queryengines/sql/metric/SQLMetricFunction.java
@@ -5,17 +5,9 @@
  */
 package com.yahoo.elide.datastores.aggregation.queryengines.sql.metric;
 
-import com.yahoo.elide.datastores.aggregation.metadata.metric.MetricFunctionInvocation;
 import com.yahoo.elide.datastores.aggregation.metadata.models.FunctionArgument;
 import com.yahoo.elide.datastores.aggregation.metadata.models.MetricFunction;
-import com.yahoo.elide.datastores.aggregation.query.ColumnProjection;
-import com.yahoo.elide.datastores.aggregation.query.TimeDimensionProjection;
-import com.yahoo.elide.datastores.aggregation.queryengines.sql.query.SQLQueryTemplate;
-import com.yahoo.elide.request.Argument;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 /**
@@ -25,38 +17,5 @@ public class SQLMetricFunction extends MetricFunction {
     public SQLMetricFunction(String name, String longName, String description, String expression,
                              Set<FunctionArgument> arguments) {
         super(name, longName, description, expression, arguments);
-    }
-
-    /**
-     * Construct a sql query template for a physical table with provided information.
-     * Table name would be filled in when convert the template into real query.
-     *
-     * @param arguments arguments provided in the request
-     * @param alias result alias
-     * @param dimensions groupBy dimensions
-     * @param timeDimension aggregated time dimension
-     * @return <code>SELECT function(arguments, fields) AS alias GROUP BY dimensions, timeDimension </code>
-     */
-    public SQLQueryTemplate resolve(Map<String, Argument> arguments,
-                                    String alias,
-                                    Set<ColumnProjection> dimensions,
-                                    TimeDimensionProjection timeDimension) {
-        MetricFunctionInvocation invoked = invoke(arguments, alias);
-        return new SQLQueryTemplate() {
-            @Override
-            public List<MetricFunctionInvocation> getMetrics() {
-                return Collections.singletonList(invoked);
-            }
-
-            @Override
-            public Set<ColumnProjection> getNonTimeDimensions() {
-                return dimensions;
-            }
-
-            @Override
-            public TimeDimensionProjection getTimeDimension() {
-                return timeDimension;
-            }
-        };
     }
 }
