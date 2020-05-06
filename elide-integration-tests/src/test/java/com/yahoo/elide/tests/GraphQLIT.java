cfd79b8a9efd998a1d8f0c458f4b4bfa3dcccc41
Sam Groth <sgroth@yahoo-inc.com>
2020-05-06 17:42:49 -0500

    Reorder imports

--- elide-integration-tests/src/test/java/com/yahoo/elide/tests/GraphQLIT.java
+++ elide-integration-tests/src/test/java/com/yahoo/elide/tests/GraphQLIT.java
@@ -30,12 +30,13 @@ import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import io.restassured.response.ValidatableResponse;
 import lombok.Getter;
 import lombok.Setter;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+
 
 import java.io.IOException;
 import java.util.ArrayList;
