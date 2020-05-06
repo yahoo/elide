633e15035f00b5ec069d163afbe89e8947c5ed89
moiz arafat <moiz.arafat@oath.com>
2020-05-05 20:35:25 -0400

    Fixing IT
    Co-authored-by: Abhino <abhino@gmail.com>

--- elide-integration-tests/src/test/java/com/yahoo/elide/initialization/LifeCycleIntegrationTestApplicationResourceConfig.java
+++ elide-integration-tests/src/test/java/com/yahoo/elide/initialization/LifeCycleIntegrationTestApplicationResourceConfig.java
@@ -3,33 +3,19 @@
  * Licensed under the Apache License, Version 2.0
  * See LICENSE file in project root for terms.
  */
-package com.yahoo.elide.async.integration.tests.framework;
+package com.yahoo.elide.initialization;
 
 import static com.yahoo.elide.annotation.LifeCycleHookBinding.Operation.CREATE;
 import static com.yahoo.elide.annotation.LifeCycleHookBinding.Operation.UPDATE;
-import static com.yahoo.elide.annotation.LifeCycleHookBinding.TransactionPhase.POSTCOMMIT;
 import static com.yahoo.elide.annotation.LifeCycleHookBinding.TransactionPhase.PRECOMMIT;
-import static com.yahoo.elide.annotation.LifeCycleHookBinding.TransactionPhase.PRESECURITY;
 
 import com.yahoo.elide.Elide;
 import com.yahoo.elide.ElideSettingsBuilder;
-import com.yahoo.elide.async.hooks.ExecuteQueryHook;
-import com.yahoo.elide.async.hooks.UpdatePrincipalNameHook;
-import com.yahoo.elide.async.integration.tests.AsyncIT;
-import com.yahoo.elide.async.models.AsyncQuery;
-import com.yahoo.elide.async.models.security.AsyncQueryOperationChecks;
-import com.yahoo.elide.async.service.AsyncCleanerService;
-import com.yahoo.elide.async.service.AsyncExecutorService;
-import com.yahoo.elide.async.service.AsyncQueryDAO;
-import com.yahoo.elide.async.service.DefaultAsyncQueryDAO;
 import com.yahoo.elide.audit.InMemoryLogger;
 import com.yahoo.elide.core.EntityDictionary;
 import com.yahoo.elide.core.filter.dialect.DefaultFilterDialect;
 import com.yahoo.elide.core.filter.dialect.MultipleFilterDialect;
 import com.yahoo.elide.core.filter.dialect.RSQLFilterDialect;
-import com.yahoo.elide.security.checks.Check;
-
-import com.google.common.collect.Lists;
 
 import example.TestCheckMappings;
 import example.models.triggers.Invoice;
@@ -42,26 +28,19 @@ import org.glassfish.jersey.server.ResourceConfig;
 
 import java.util.Arrays;
 import java.util.Calendar;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import javax.inject.Inject;
 
-public class AsyncIntegrationTestApplicationResourceConfig extends ResourceConfig {
+public class LifeCycleIntegrationTestApplicationResourceConfig extends ResourceConfig {
     public static final InMemoryLogger LOGGER = new InMemoryLogger();
 
     @Inject
-    public AsyncIntegrationTestApplicationResourceConfig(ServiceLocator injector) {
+    public LifeCycleIntegrationTestApplicationResourceConfig(ServiceLocator injector) {
         // Bind to injector
         register(new AbstractBinder() {
             @Override
             protected void configure() {
-                Map<String, Class<? extends Check>> checkMappings = new HashMap<>(TestCheckMappings.MAPPINGS);
-                checkMappings.put(AsyncQueryOperationChecks.AsyncQueryOwner.PRINCIPAL_IS_OWNER, AsyncQueryOperationChecks.AsyncQueryOwner.class);
-                checkMappings.put(AsyncQueryOperationChecks.AsyncQueryStatusValue.VALUE_IS_CANCELLED, AsyncQueryOperationChecks.AsyncQueryStatusValue.class);
-
-                EntityDictionary dictionary = new EntityDictionary(checkMappings, injector::inject);
+                EntityDictionary dictionary = new EntityDictionary(TestCheckMappings.MAPPINGS, injector::inject);
 
                 bind(dictionary).to(EntityDictionary.class);
 
@@ -73,7 +52,7 @@ public class AsyncIntegrationTestApplicationResourceConfig extends ResourceConfi
                         Arrays.asList(rsqlFilterStrategy, defaultFilterStrategy)
                 );
 
-                Elide elide = new Elide(new ElideSettingsBuilder(AsyncIT.getDataStore())
+                Elide elide = new Elide(new ElideSettingsBuilder(IntegrationTest.getDataStore())
                         .withAuditLogger(LOGGER)
                         .withJoinFilterDialect(multipleFilterStrategy)
                         .withSubqueryFilterDialect(multipleFilterStrategy)
@@ -82,43 +61,19 @@ public class AsyncIntegrationTestApplicationResourceConfig extends ResourceConfi
                         .build());
                 bind(elide).to(Elide.class).named("elide");
 
-                AsyncQueryDAO asyncQueryDao = new DefaultAsyncQueryDAO(elide, elide.getDataStore());
-                bind(asyncQueryDao).to(AsyncQueryDAO.class);
-
-                AsyncExecutorService.init(elide, 5, 60, asyncQueryDao);
-                bind(AsyncExecutorService.getInstance()).to(AsyncExecutorService.class);
-
                 BillingService billingService = new BillingService() {
                     @Override
                     public long purchase(Invoice invoice) {
-                        return 0;
+                        return 100;
                     }
                 };
 
                 bind(billingService).to(BillingService.class);
 
-                // Binding AsyncQuery LifeCycleHook
-                ExecuteQueryHook executeQueryHook = new ExecuteQueryHook(AsyncExecutorService.getInstance());
-                UpdatePrincipalNameHook updatePrincipalNameHook = new UpdatePrincipalNameHook();
                 InvoiceCompletionHook invoiceCompletionHook = new InvoiceCompletionHook(billingService);
-
-                dictionary.bindTrigger(AsyncQuery.class, CREATE, POSTCOMMIT, executeQueryHook, false);
-                dictionary.bindTrigger(AsyncQuery.class, CREATE, PRESECURITY, updatePrincipalNameHook, false);
                 dictionary.bindTrigger(Invoice.class, "complete", CREATE, PRECOMMIT, invoiceCompletionHook);
                 dictionary.bindTrigger(Invoice.class, "complete", UPDATE, PRECOMMIT, invoiceCompletionHook);
-
-                AsyncCleanerService.init(elide, 60, 5, asyncQueryDao);
-                bind(AsyncCleanerService.getInstance()).to(AsyncCleanerService.class);
             }
         });
-
-        registerFilters(Lists.newArrayList(AsyncAuthFilter.class));
-    }
-
-    /**
-     * Register provided JAX-RS filters.
-     */
-    private void registerFilters(List<Class<?>> filters) {
-        filters.forEach(this::register);
     }
 }
--- elide-integration-tests/src/test/java/com/yahoo/elide/triggers/LifeCycleHookIT.java
+++ elide-integration-tests/src/test/java/com/yahoo/elide/triggers/LifeCycleHookIT.java
@@ -13,30 +13,26 @@ import static com.yahoo.elide.contrib.testhelpers.jsonapi.JsonApiDSL.datum;
 import static com.yahoo.elide.contrib.testhelpers.jsonapi.JsonApiDSL.id;
 import static com.yahoo.elide.contrib.testhelpers.jsonapi.JsonApiDSL.resource;
 import static com.yahoo.elide.contrib.testhelpers.jsonapi.JsonApiDSL.type;
-import static com.yahoo.elide.initialization.StandardTestBinder.BILLING_SERVICE;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.reset;
 
 import com.yahoo.elide.core.HttpStatus;
 import com.yahoo.elide.initialization.IntegrationTest;
+import com.yahoo.elide.initialization.LifeCycleIntegrationTestApplicationResourceConfig;
+import com.yahoo.elide.resources.JsonApiEndpoint;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
+import org.junit.jupiter.api.TestInstance;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class LifeCycleHookIT extends IntegrationTest {
 
-    @BeforeEach
-    public void resetMocks() {
-        reset(BILLING_SERVICE);
+    public LifeCycleHookIT() {
+        super(LifeCycleIntegrationTestApplicationResourceConfig.class, JsonApiEndpoint.class.getPackage().getName());
     }
 
     @Test
     public void testBillingServiceInvocation() {
-
-        doReturn(100L).when(BILLING_SERVICE).purchase(ArgumentMatchers.any());
 
         given()
                 .contentType(JSONAPI_CONTENT_TYPE)
