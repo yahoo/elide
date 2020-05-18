d9d00e1204bf4c2ffdb9caf0f5d6afda5c496793
Ramsha Rao <ramsha.rao@verizonmedia.com>
2020-05-18 19:40:09 +0000

    TANDS-19093-addressing-comments

--- elide-core/src/main/java/com/yahoo/elide/core/TransactionRegistry.java
+++ elide-core/src/main/java/com/yahoo/elide/core/TransactionRegistry.java
@@ -5,21 +5,37 @@
  */
 package com.yahoo.elide.core;
 import java.util.Set;
+import lombok.Data;
 /**
- * Database interface library.
+ * Transaction Registry interface to surface transaction details to other parts of Elide.
  */
 public interface TransactionRegistry {
-
+    @Data
     public static class TransactionEntry {
         public RequestScope request;
         public DataStoreTransaction transaction;
     }
-
+    
+    /**
+     * @return all running transactions
+     */
     Set<TransactionEntry> getRunningTransactions();
 
+    /**
+     * @param requestId
+     * @return matching running transaction
+     */
     Set<TransactionEntry> getRunningTransaction(String requestId);
 
+    /**
+     * Adds running transaction
+     * @param transactionEntry TransactionEntry transactionEntry
+     */
     void addRunningTransaction(TransactionEntry transactionEntry);
-
+    
+    /**
+     * Removes running transaction when we call cancel on it
+     * @param transactionEntry TransactionEntry transactionEntry
+     */
     void removeRunningTransaction(TransactionEntry transactionEntry);
 }
