cbc2fb07894b6ba9bfc3db67eddfa22b24f88768
Ramsha Rao <ramsha.rao@verizonmedia.com>
2020-05-18 20:28:06 +0000

    TANDS-19093-addressing-comments

--- elide-core/src/main/java/com/yahoo/elide/core/TransactionRegistry.java
+++ elide-core/src/main/java/com/yahoo/elide/core/TransactionRegistry.java
@@ -18,7 +18,6 @@ public interface TransactionRegistry {
          * @see RequestScope
          * @see DataStoreTransaction 
          */
-
         public RequestScope request;
         public DataStoreTransaction transaction;
     }
