08f573c522cb3d328b4c57a43d97947b2683caa5
Ramsha Rao <ramsha.rao@verizonmedia.com>
2020-05-18 21:32:58 +0000

    TANDS-19093-transactionRegistry-interface

--- elide-core/src/main/java/com/yahoo/elide/core/TransactionRegistry.java
+++ elide-core/src/main/java/com/yahoo/elide/core/TransactionRegistry.java
@@ -1,47 +1,47 @@
 /*
- *  * Copyright 2020, Yahoo Inc.
- *   * Licensed under the Apache License, Version 2.0
- *    * See LICENSE file in project root for terms.
- *     */
+ * Copyright 2020, Yahoo Inc.
+ * Licensed under the Apache License, Version 2.0
+ * See LICENSE file in project root for terms.
+ */
 package com.yahoo.elide.core;
 
 import lombok.Data;
 
 import java.util.Set;
 /**
- *  * Transaction Registry interface to surface transaction details to other parts of Elide.
- *   */
+ * Transaction Registry interface to surface transaction details to other parts of Elide.
+ */
 public interface TransactionRegistry {
     @Data
     /**
- *      * @see RequestScope  
- *           * @see DataStoreTransaction      
- *                */
+     * @see RequestScope  
+     * @see DataStoreTransaction      
+     */
     public static class TransactionEntry {
         public RequestScope request;
         public DataStoreTransaction transaction;
     }
 
     /**
- *      * @return all running transactions
- *           */
+     * @return all running transactions
+     */
     Set<TransactionEntry> getRunningTransactions();
 
     /**
- *      * @param requestId
- *           * @return matching running transaction
- *                */
+     * @param requestId
+     * @return matching running transaction
+     */
     Set<TransactionEntry> getRunningTransaction(String requestId);
 
     /**
- *      * Adds running transaction
- *           * @param transactionEntry TransactionEntry transactionEntry
- *                */
+     * Adds running transaction
+     * @param transactionEntry TransactionEntry transactionEntry
+     */
     void addRunningTransaction(TransactionEntry transactionEntry);
 
     /**
- *      * Removes running transaction when we call cancel on it
- *           * @param transactionEntry TransactionEntry transactionEntry
- *                */
+     * Removes running transaction when we call cancel on it 
+     * @param transactionEntry TransactionEntry transactionEntry
+     */
     void removeRunningTransaction(TransactionEntry transactionEntry);
 }
