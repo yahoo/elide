07405d19f9ba68f0ed99a808a7c2a9c7840c33d6
Ramsha Rao <ramsha.rao@verizonmedia.com>
2020-05-21 15:22:16 +0000

    TANDS-19169-fixing-spaces

--- elide-core/src/main/java/com/yahoo/elide/core/datastore/inmemory/InMemoryTransaction.java
+++ elide-core/src/main/java/com/yahoo/elide/core/datastore/inmemory/InMemoryTransaction.java
@@ -3,7 +3,6 @@
  * Licensed under the Apache License, Version 2.0
  * See LICENSE file in project root for terms.
  */
-
 package com.yahoo.elide.core.datastore.inmemory;
 import com.yahoo.elide.core.TransactionRegistry;
 
