3acf50183fae333e08e7313ae907684146bd6082
Ramsha Rao <ramsha.rao@verizonmedia.com>
2020-05-21 15:18:57 +0000

    TANDS-19169-fixing-spaces

--- elide-core/src/main/java/com/yahoo/elide/core/datastore/inmemory/InMemoryTransaction.java
+++ elide-core/src/main/java/com/yahoo/elide/core/datastore/inmemory/InMemoryTransaction.java
@@ -14,7 +14,7 @@ import java.util.Set;
  */
 public class InMemoryTransaction implements TransactionRegistry {
     private TransactionRegistry registry = new TransactionRegistry();
-    
+
     @Override
     public Set<TransactionEntry> getRunningTransactions() {
         registry.getRunningTransactions();
