536fd85a5d8b90f088852d603a56790a0a3f31fd
Ramsha Rao <ramsha.rao@verizonmedia.com>
2020-05-21 22:11:44 +0000

    TANDS-19169

--- elide-core/src/main/java/com/yahoo/elide/Elide.java
+++ elide-core/src/main/java/com/yahoo/elide/Elide.java
@@ -57,7 +57,6 @@ import lombok.extern.slf4j.Slf4j;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
