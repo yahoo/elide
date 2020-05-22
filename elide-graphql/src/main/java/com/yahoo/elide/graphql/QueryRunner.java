830f71c63842161ac3b1e96e884e418fc88c494e
Ramsha Rao <ramsha.rao@verizonmedia.com>
2020-05-22 16:32:55 +0000

    addTransaction-removeTransaction

--- elide-graphql/src/main/java/com/yahoo/elide/graphql/QueryRunner.java
+++ elide-graphql/src/main/java/com/yahoo/elide/graphql/QueryRunner.java
@@ -41,6 +41,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.UUID;
 import java.util.function.Function;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
@@ -144,8 +145,10 @@ public class QueryRunner {
     private ElideResponse executeGraphQLRequest(ObjectMapper mapper, User principal,
                                                 String graphQLDocument, JsonNode jsonDocument) {
         boolean isVerbose = false;
+        UUID requestId = null;
         try (DataStoreTransaction tx = elide.getDataStore().beginTransaction()) {
-
+            requestId = tx.getId();
+            elide.getTransactionRegistry().addRunningTransaction(requestId, tx); 
             if (!jsonDocument.has(QUERY)) {
                 return ElideResponse.builder()
                         .responseCode(HttpStatus.SC_BAD_REQUEST)
@@ -270,6 +273,7 @@ public class QueryRunner {
             log.error("Unhandled error or exception.", e);
             throw e;
         } finally {
+            elide.getTransactionRegistry().removeRunningTransaction(requestId);
             elide.getAuditLogger().clear();
         }
     }
