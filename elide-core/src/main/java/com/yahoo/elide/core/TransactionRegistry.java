c04c0534c5734751b60a2f3b6991e091f31aa039
Ramsha Rao <ramsha.rao@verizonmedia.com>
2020-05-22 18:29:26 +0000

    addressing-comments

---
+++ elide-core/src/main/java/com/yahoo/elide/core/TransactionRegistry.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2020, Yahoo Inc.
+ * Licensed under the Apache License, Version 2.0
+ * See LICENSE file in project root for terms.
+ */
+package com.yahoo.elide.core;
+
+import lombok.Data;
+
+import java.util.HashMap;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+/**
+* Transaction Registry class.
+*/
+@Getter
+public static class TransactionRegistry {
+    private Map<UUID, DataStoreTransaction> transactionMap = new HashMap<>();
+    public Set<DataStoreTransaction> getRunningTransactions() {
+        Set<DataStoreTransaction> transactions = transactionMap.values().stream().collect(Collectors.toSet());
+        return transactions;
+    }
+
+    public DataStoreTransaction getRunningTransaction(UUID requestId) {
+        return transactionMap.get(requestId);
+    }
+
+    public void addRunningTransaction(UUID requestId, DataStoreTransaction tx) {
+        transactionMap.put(requestId, tx);
+    }
+
+    public void removeRunningTransaction(UUID requestId) {
+        transactionMap.remove(requestId);
+    }
+}
