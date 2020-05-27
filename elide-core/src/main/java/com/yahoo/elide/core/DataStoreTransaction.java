862dd9c905d40ca5d0a94ce99ba77880624b3d52
Ramsha Rao <ramsha.rao@verizonmedia.com>
2020-05-27 18:58:01 +0000

    fixing bugs

--- elide-core/src/main/java/com/yahoo/elide/core/DataStoreTransaction.java
+++ elide-core/src/main/java/com/yahoo/elide/core/DataStoreTransaction.java
@@ -17,7 +17,7 @@ import java.io.Closeable;
 import java.io.Serializable;
 import java.util.Iterator;
 import java.util.Set;
-
+import java.util.UUID;
 /**
  * Wraps the Database Transaction type.
  */
