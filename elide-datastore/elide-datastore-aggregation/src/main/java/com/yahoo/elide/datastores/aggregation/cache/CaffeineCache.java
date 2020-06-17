5059f5f2e580bb5efe88b8b6d7f93e8c80e13a1b
John Karp <john-karp@users.noreply.github.com>
2020-06-17 14:08:31 -0500

    Enable query caching by default (#1378)
    * Create new cache package
    
    * Add caffeine cache based impl
    
    * Configure Caffeine cache by default
    
    * Publish cache metrics under Spring/micrometer
    
    * Lint
    
    * Fix javadoc
    
    * Rename max cache size for clarity
    
    * Update ElideAutoConfiguration.java
    
    Co-authored-by: Aaron Klish <aklish@gmail.com>

---
+++ elide-datastore/elide-datastore-aggregation/src/main/java/com/yahoo/elide/datastores/aggregation/cache/CaffeineCache.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2020, Yahoo Inc.
+ * Licensed under the Apache License, Version 2.0
+ * See LICENSE file in project root for terms.
+ */
+
+package com.yahoo.elide.datastores.aggregation.cache;
+
+import com.yahoo.elide.datastores.aggregation.query.QueryResult;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+
+/**
+ * A basic local-only cache.
+ */
+public class CaffeineCache implements Cache {
+    public static final int DEFAULT_MAXIMUM_ENTRIES = 1024;
+
+    private final com.github.benmanes.caffeine.cache.Cache<Object, QueryResult> cache;
+
+    public CaffeineCache(int maximumSize) {
+        cache = Caffeine.newBuilder().maximumSize(maximumSize).recordStats().build();
+    }
+
+    @Override
+    public QueryResult get(Object key) {
+        return cache.getIfPresent(key);
+    }
+
+    @Override
+    public void put(Object key, QueryResult result) {
+        cache.put(key, result);
+    }
+
+    public com.github.benmanes.caffeine.cache.Cache<Object, QueryResult> getImplementation() {
+        return cache;
+    }
+}
