bad6e8d0354e4cdc8446e55bdc9362bc036d9a26
Moiz Arafat <moiz.arafat@verizonmedia.com>
2020-08-07 10:22:43 -0400

    Graphql Query Default Pagination Setup (#1478)
    * Ensuring Default Pagination for Graphql Projections
    
    * Test case

---
+++ elide-graphql/src/test/java/com/yahoo/elide/graphql/parser/GraphQLEntityProjectMakerTest.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2020, Yahoo Inc.
+ * Licensed under the Apache License, Version 2.0
+ * See LICENSE file in project root for terms.
+ */
+package com.yahoo.elide.graphql.parser;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.yahoo.elide.graphql.PersistentResourceFetcherTest;
+import com.yahoo.elide.request.Pagination;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class GraphQLEntityProjectMakerTest extends PersistentResourceFetcherTest {
+
+     @Test
+     public void testDefaultPagination() throws IOException {
+         String graphQLRequest = loadGraphQLRequest("fetch/rootSingle" + ".graphql");
+         GraphQLEntityProjectionMaker projectionMaker = new GraphQLEntityProjectionMaker(settings);
+         GraphQLProjectionInfo projectionInfo = projectionMaker.make(graphQLRequest);
+         assertEquals(projectionInfo.getProjections().size(), 1);
+         assertEquals(projectionInfo.getProjections().entrySet().iterator().next().getValue().getPagination().isDefaultInstance(), true);
+     }
+
+     @Test
+     public void testLimitPagination() throws IOException {
+         String graphQLRequest = loadGraphQLRequest("fetch/rootCollectionPaginateWithOffset" + ".graphql");
+         GraphQLEntityProjectionMaker projectionMaker = new GraphQLEntityProjectionMaker(settings);
+         GraphQLProjectionInfo projectionInfo = projectionMaker.make(graphQLRequest);
+         assertEquals(projectionInfo.getProjections().size(), 1);
+         Pagination page = projectionInfo.getProjections().entrySet().iterator().next().getValue().getPagination();
+         assertEquals(page.isDefaultInstance(), false);
+         assertEquals(page.getLimit(), 1);
+     }
+}
