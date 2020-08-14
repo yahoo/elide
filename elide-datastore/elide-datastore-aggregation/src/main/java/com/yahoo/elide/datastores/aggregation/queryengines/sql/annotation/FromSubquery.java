399b5af1b5d02b3fb3137cc9fd194611c81aa3b2
Moiz Arafat <moiz.arafat@verizonmedia.com>
2020-08-14 10:49:22 -0400

    Fixing the schema (#1501)
    Co-authored-by: AvaniMakwana <Avani.makwana@outlook.com>
    
    Co-authored-by: AvaniMakwana <Avani.makwana@outlook.com>

--- elide-datastore/elide-datastore-aggregation/src/main/java/com/yahoo/elide/datastores/aggregation/queryengines/sql/annotation/FromSubquery.java
+++ elide-datastore/elide-datastore-aggregation/src/main/java/com/yahoo/elide/datastores/aggregation/queryengines/sql/annotation/FromSubquery.java
@@ -7,6 +7,7 @@ package com.yahoo.elide.datastores.aggregation.queryengines.sql.annotation;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -17,6 +18,7 @@ import java.lang.annotation.Target;
 @Documented
 @Target({ElementType.TYPE, ElementType.METHOD, ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
+@Inherited
 public @interface FromSubquery {
 
     /**
