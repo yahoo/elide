e3dc786a24464d0fe0d0524459a5abe3623f6045
HarveyC3 <harvey.chong@gmail.com>
2020-08-21 16:27:27 -0500

    Add SQLDialect implementations and tests. (#1487)
    * Add SQLDialect implementations and tests.
    
    * Cleanup based on codacy feedback
    
    * fix * import
    
    * Merge showQueries() into explain()
    
    * Add copyright statements and class javadoc entries
    
    * Remove * import in PrestoExplainQueryTest
    
    * Rework TestQuery enum.
    Change GROUP_BY_NOT_IN_SELECT to ORDER_BY_NOT_IN_SELECT.
    
    * Fix checkstyle errors
    
    * Update QueryEngine.java
    
    * Add setHighScoreNoAgg() in PlayerStats.java
    
    * Add method comments to SQLDialect. Don't throw RuntimeException in SQLUnitTest.
    
    Co-authored-by: Harvey Chong <harveyc@verizonmedia.com>
    Co-authored-by: Aaron Klish <aklish@gmail.com>

--- elide-datastore/elide-datastore-aggregation/src/main/java/com/yahoo/elide/datastores/aggregation/queryengines/sql/query/SQLQueryConstructor.java
+++ elide-datastore/elide-datastore-aggregation/src/main/java/com/yahoo/elide/datastores/aggregation/queryengines/sql/query/SQLQueryConstructor.java
@@ -27,6 +27,7 @@ import com.yahoo.elide.datastores.aggregation.metadata.models.TimeDimension;
 import com.yahoo.elide.datastores.aggregation.query.Query;
 import com.yahoo.elide.datastores.aggregation.queryengines.sql.annotation.FromSubquery;
 import com.yahoo.elide.datastores.aggregation.queryengines.sql.annotation.FromTable;
+import com.yahoo.elide.datastores.aggregation.queryengines.sql.dialects.SQLDialect;
 import com.yahoo.elide.datastores.aggregation.queryengines.sql.metadata.SQLReferenceTable;
 import com.yahoo.elide.request.Argument;
 import com.yahoo.elide.request.Sorting;
@@ -50,10 +51,12 @@ import java.util.stream.Stream;
 public class SQLQueryConstructor {
     private final SQLReferenceTable referenceTable;
     private final EntityDictionary dictionary;
+    private final SQLDialect dialect;
 
-    public SQLQueryConstructor(SQLReferenceTable referenceTable) {
+    public SQLQueryConstructor(SQLReferenceTable referenceTable, SQLDialect sqlDialect) {
         this.referenceTable = referenceTable;
         this.dictionary = referenceTable.getDictionary();
+        this.dialect = sqlDialect;
     }
 
     /**
