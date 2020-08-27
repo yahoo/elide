ecb1fb18fc0ca7ee3c47924bd5cdcc8d25d8f06c
rishi-aga <61517466+rishi-aga@users.noreply.github.com>
2020-08-27 15:12:40 -0400

    Include db connection name as Id for Table (#1503)
    * Support dynamic configuration for db connections
    
    * Fixes for reading from JAR file
    
    * Create jar with dependencies for config validation
    
    * Review Comments
    
    * Codacy check
    
    * Review comment for using Named interface
    
    * Review comment for using Named interface
    
    * Review comment for using Named interface
    
    * Remove extra code
    
    * Review comment for using Named interface
    
    * Review comment for using Named interface
    
    * Review Comment
    
    * Include db connection name as Id for Table
    
    * Review Comments
    
    * Update TableId.java
    
    Co-authored-by: Aaron Klish <aklish@gmail.com>

---
+++ elide-datastore/elide-datastore-aggregation/src/main/java/com/yahoo/elide/datastores/aggregation/metadata/models/TableId.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2020, Yahoo Inc.
+ * Licensed under the Apache License, Version 2.0
+ * See LICENSE file in project root for terms.
+ */
+package com.yahoo.elide.datastores.aggregation.metadata.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+
+/**
+ * Composite ID for Table entity.
+ */
+@SuppressWarnings("serial")
+@Embeddable
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TableId implements Serializable {
+
+    private String name;
+    private String version;
+    private String dbConnectionName;
+
+    @Override
+    public String toString() {
+        return name + prefix(version, ".") + prefix(dbConnectionName, "-");
+    }
+
+    private String prefix(String str, String prefix) {
+        return (str == null || str.trim().isEmpty()) ? "" : prefix + str;
+    }
+}
