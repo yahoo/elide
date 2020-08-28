d0f84ccbf411c1867b38c19a15b78a7ae7b017b5
Moiz Arafat <moiz.arafat@verizonmedia.com>
2020-08-28 13:00:44 -0400

    Time Dimension should support different formats during serialization (#1511)
    * date and datetime grains
    
    * Handlebar and Schema changes for TimeGrain
    
    * Table Schema Change for Elide Spring Test
    
    * Change Type definition
    
    * Time Grain Changes
    
    * Remove FORMATDATETIME
    
    * Removing commented code
    
    * Rename Date to SimpleDate
    
    * Renaming Grain
    
    * Some testing
    
    * Working
    
    * Adding format getters
    
    * Additional Serdes
    
    * Schema changes
    
    * Update GraphQLConversionUtils.java
    
    * Review Comments
    
    * Remove extra
    
    * Codacy Issues
    
    * Codacy Issues
    
    * Fix test HJSONs
    
    * Codacy issue and remove extra file under resources
    
    Co-authored-by: Ayeswarya Anandan <aanandan@vpn-pool-57-26.buffalo.office.oath>
    Co-authored-by: rishi-aga <rishi.aga123@gmail.com>

---
+++ elide-datastore/elide-datastore-aggregation/src/main/java/com/yahoo/elide/datastores/aggregation/timegrains/serde/DateTimeSerde.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2020, Yahoo Inc.
+ * Licensed under the Apache License, Version 2.0
+ * See LICENSE file in project root for terms.
+ */
+package com.yahoo.elide.datastores.aggregation.timegrains.serde;
+
+import com.yahoo.elide.datastores.aggregation.metadata.enums.TimeGrain;
+import com.yahoo.elide.datastores.aggregation.timegrains.DateTime;
+import com.yahoo.elide.utils.coerce.converters.ElideTypeConverter;
+import com.yahoo.elide.utils.coerce.converters.Serde;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Serde class for bidirectional conversion from Elide DateTime type to java.util.Date.
+ */
+@ElideTypeConverter(type = DateTime.class, name = "DateTime")
+public class DateTimeSerde implements Serde<Object, Date> {
+
+    private static final SimpleDateFormat DATETIME_FORMATTER = new SimpleDateFormat(TimeGrain.DATETIME.getFormat());
+
+    @Override
+    public Date deserialize(Object val) {
+
+        Date date = null;
+
+        try {
+            if (val instanceof String) {
+                date = new Date(DATETIME_FORMATTER.parse((String) val).getTime());
+            } else {
+                date = new DateTime(DATETIME_FORMATTER.parse(DATETIME_FORMATTER.format(val)));
+            }
+        } catch (ParseException e) {
+            throw new IllegalArgumentException("Date strings must be formated as " + DATETIME_FORMATTER.toPattern());
+        }
+
+        return date;
+    }
+
+    @Override
+    public String serialize(Date val) {
+        return DATETIME_FORMATTER.format(val).toString();
+    }
+}
