b930e6d4e01b319a54e418c20b6ea787c0be1c89
Aaron Klish <klish@verizonmedia.com>
2020-09-08 15:39:53 -0500

    Added unit tests

--- elide-datastore/elide-datastore-aggregation/src/test/java/com/yahoo/elide/datastores/aggregation/filter/visitor/MatchesTemplateVisitorTest.java
+++ elide-datastore/elide-datastore-aggregation/src/test/java/com/yahoo/elide/datastores/aggregation/filter/visitor/MatchesTemplateVisitorTest.java
@@ -11,12 +11,12 @@ import com.yahoo.elide.core.filter.dialect.RSQLFilterDialect;
 import com.yahoo.elide.core.filter.expression.FilterExpression;
 import com.yahoo.elide.datastores.aggregation.example.Player;
 import com.yahoo.elide.datastores.aggregation.example.PlayerStats;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
@@ -32,8 +32,75 @@ public class MatchesTemplateVisitorTest {
     }
 
     @Test
-    public void exactMatchTest() throws Exception {
+    public void predicateMatchTest() throws Exception {
         FilterExpression clientExpression = dialect.parseFilterExpression("highScore==123",
+                PlayerStats.class, true);
+
+        FilterExpression templateExpression = dialect.parseFilterExpression("highScore==${variable}",
+                PlayerStats.class, false, true);
+
+        assertTrue(MatchesTemplateVisitor.isValid(templateExpression, clientExpression));
+    }
+
+    @Test
+    public void conjunctionMatchTest() throws Exception {
+        FilterExpression clientExpression = dialect.parseFilterExpression("lowScore>100;highScore==123",
+                PlayerStats.class, true);
+
+        FilterExpression templateExpression = dialect.parseFilterExpression("highScore==${variable}",
+                PlayerStats.class, false, true);
+
+        assertTrue(MatchesTemplateVisitor.isValid(templateExpression, clientExpression));
+    }
+
+    @Test
+    public void conjunctionMismatchTest() throws Exception {
+        FilterExpression clientExpression = dialect.parseFilterExpression("lowScore>100;player.name==Bob*",
+                PlayerStats.class, true);
+
+        FilterExpression templateExpression = dialect.parseFilterExpression("highScore==${variable}",
+                PlayerStats.class, false, true);
+
+        assertFalse(MatchesTemplateVisitor.isValid(templateExpression, clientExpression));
+    }
+
+    @Test
+    public void disjunctionMatchTest() throws Exception {
+        FilterExpression clientExpression = dialect.parseFilterExpression("lowScore>100,highScore==123",
+                PlayerStats.class, true);
+
+        FilterExpression templateExpression = dialect.parseFilterExpression("highScore==${variable}",
+                PlayerStats.class, false, true);
+
+        assertFalse(MatchesTemplateVisitor.isValid(templateExpression, clientExpression));
+    }
+
+    @Test
+    public void disjunctionMismatchTest() throws Exception {
+        FilterExpression clientExpression = dialect.parseFilterExpression("lowScore>100,player.name==Bob*",
+                PlayerStats.class, true);
+
+        FilterExpression templateExpression = dialect.parseFilterExpression("highScore==${variable}",
+                PlayerStats.class, false, true);
+
+        assertFalse(MatchesTemplateVisitor.isValid(templateExpression, clientExpression));
+    }
+
+    @Test
+    public void predicateMismatchTest() throws Exception {
+        FilterExpression clientExpression = dialect.parseFilterExpression("highScore!=123",
+                PlayerStats.class, true);
+
+        FilterExpression templateExpression = dialect.parseFilterExpression("highScore==${variable}",
+                PlayerStats.class, false, true);
+
+        assertFalse(MatchesTemplateVisitor.isValid(templateExpression, clientExpression));
+    }
+
+    @Test
+    public void complexExpressionTest() throws Exception {
+        String complexExpression = "(lowScore>100;((player.name==Bob*,lowScore>100);(highScore==123)))";
+        FilterExpression clientExpression = dialect.parseFilterExpression(complexExpression,
                 PlayerStats.class, true);
 
         FilterExpression templateExpression = dialect.parseFilterExpression("highScore==${variable}",
