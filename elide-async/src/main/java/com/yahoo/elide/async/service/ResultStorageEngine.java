ab6c7eecb9a14f056d44341db06b90d3f7811924
Moiz Arafat <moiz.arafat@verizonmedia.com>
2020-09-10 14:05:37 -0400

    Result Storage Engine Implementation (#1526)
    * Result Storage Engine Implementation
    
    Co-authored-by: chandrakaran <chandrakaranreddy.patlolla1@gmail.com>
    
    * Result Storage Engine Implementation
    
    Co-authored-by: chandrakaran <chandrakaranreddy.patlolla1@gmail.com>
    
    * Review comments
    
    Co-authored-by: chandrakaran <chandrakaranreddy.patlolla1@gmail.com>
    
    * Review comments
    
    Co-authored-by: chandrakaran <chandrakaranreddy.patlolla1@gmail.com>
    
    * Review comments.
    
    Co-authored-by: chandrakaran <chandrakaranreddy.patlolla1@gmail.com>
    
    Co-authored-by: chandrakaran <chandrakaranreddy.patlolla1@gmail.com>

---
+++ elide-async/src/main/java/com/yahoo/elide/async/service/ResultStorageEngine.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2020, Yahoo Inc.
+ * Licensed under the Apache License, Version 2.0
+ * See LICENSE file in project root for terms.
+ */
+
+package com.yahoo.elide.async.service;
+
+import com.yahoo.elide.async.models.AsyncQuery;
+
+import io.reactivex.Observable;
+
+/**
+ * Utility interface used for storing the results of AsyncQuery for downloads.
+ */
+public interface ResultStorageEngine {
+
+    /**
+     * Stores the result of the query.
+     * @param asyncQuery AsyncQuery object
+     * @param result is the observable result obtained by running the query
+     * @return String to store as attachment. Can be null.
+     */
+    public AsyncQuery storeResults(AsyncQuery asyncQuery, Observable<String> result);
+
+    /**
+     * Searches for the async query results by ID and returns the record.
+     * @param asyncQueryID is the query ID of the AsyncQuery
+     * @return returns the result associated with the AsyncQueryID
+     */
+    public Observable<String> getResultsByID(String asyncQueryID);
+}
---
+++ elide-async/src/test/java/com/yahoo/elide/async/service/FileResultStorageEngineTest.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2020, Yahoo Inc.
+ * Licensed under the Apache License, Version 2.0
+ * See LICENSE file in project root for terms.
+ */
+package com.yahoo.elide.async.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.yahoo.elide.async.models.AsyncQuery;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import io.reactivex.Observable;
+
+import java.io.File;
+import java.nio.file.Path;
+
+/**
+ * Test cases for FileResultStorageEngine.
+ */
+public class FileResultStorageEngineTest {
+    private static final String BASE_PATH = "src/test/resources/downloads/";
+
+    @Test
+    public void testRead() {
+        String finalResult = readResultsFile(BASE_PATH, "non_empty_results");
+        assertEquals(finalResult, "test");
+    }
+
+    @Test
+    public void testReadEmptyFile() {
+        String finalResult = readResultsFile(BASE_PATH, "empty_results");
+        assertEquals(finalResult, "");
+    }
+
+    @Test
+    public void testReadNonExistentFile() {
+        assertThrows(IllegalStateException.class, () ->
+                readResultsFile(BASE_PATH , "nonexisting_results")
+        );
+    }
+
+    @Test
+    public void testStoreResults(@TempDir Path tempDir) {
+        String queryId = "store_results_success";
+        String validOutput = "hi\nhello";
+        String[] input = validOutput.split("\n");
+
+        storeResultsFile(tempDir.toString(), queryId, Observable.fromArray(input));
+
+        File file = new File(tempDir.toString() + File.separator + queryId);
+        assertTrue(file.exists());
+
+        // verify contents of stored files are readable and match original
+        String finalResult = readResultsFile(tempDir.toString(), queryId);
+        assertEquals(finalResult, validOutput);
+    }
+
+    // O/P Directory does not exist.
+    @Test
+    public void testStoreResultsFail(@TempDir File tempDir) {
+        assertThrows(IllegalStateException.class, () ->
+                storeResultsFile(tempDir.toString() + "invalid", "store_results_fail",
+                        Observable.fromArray(new String[]{"hi", "hello"}))
+        );
+    }
+
+    private String readResultsFile(String path, String queryId) {
+        FileResultStorageEngine engine = new FileResultStorageEngine(path);
+
+        return engine.getResultsByID(queryId).collect(() -> new StringBuilder(),
+                (resultBuilder, tempResult) -> {
+                    if (resultBuilder.length() > 0) {
+                        resultBuilder.append(System.getProperty("line.separator"));
+                    }
+                    resultBuilder.append(tempResult);
+                }
+            ).map(StringBuilder::toString).blockingGet();
+    }
+
+    private void storeResultsFile(String path, String queryId, Observable<String> storable) {
+        FileResultStorageEngine engine = new FileResultStorageEngine(path);
+        AsyncQuery query = new AsyncQuery();
+        query.setId(queryId);
+
+        engine.storeResults(query, storable);
+    }
+}
