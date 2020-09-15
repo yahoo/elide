b455adefbeb70d7bde5c67df3aaf4674e3bef79f
Aaron Klish <aklish@gmail.com>
2020-09-15 16:06:14 -0500

    Add column enum values for Aggregation Store metadata (#1531)
    * Added support to provide a list of values for a column that is an enum
    
    * Refactored dynamic config schema and metadata for tableSource and values
    
    * Added a test case
    
    * Added handlebar tests
    
    * Added IT tests
    
    * Remove values from measure
    
    * Fixed checkstyle error
    
    * Added tag support to metadata (#1538)
    
    Co-authored-by: Aaron Klish <klish@verizonmedia.com>
    
    Co-authored-by: Aaron Klish <klish@verizonmedia.com>

---
+++ elide-datastore/elide-datastore-aggregation/src/main/java/com/yahoo/elide/datastores/aggregation/annotation/ColumnMeta.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2019, Yahoo Inc.
+ * Licensed under the Apache License, Version 2.0
+ * See LICENSE file in project root for terms.
+ */
+package com.yahoo.elide.datastores.aggregation.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Indicates that the specified entity field has a configured long name and field description for human to read on UI.
+ */
+@Documented
+@Target({ElementType.FIELD, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ColumnMeta {
+
+    String description() default "";
+    String category() default "";
+    String tableSource() default "";
+    String [] tags() default {};
+    String [] values() default {};
+}
