b8453936a798e72c6c44fdca3b75b66dcf782e27
moiz arafat <moiz.arafat@verizonmedia.com>
2020-09-17 10:47:23 -0400

    Test fix
    Co-authored-by: chandrakaran <chandrakaranreddy.patlolla1@gmail.com>

--- elide-async/src/test/java/com/yahoo/elide/async/service/AsyncQueryThreadTest.java
+++ elide-async/src/test/java/com/yahoo/elide/async/service/AsyncQueryThreadTest.java
@@ -6,7 +6,6 @@
 package com.yahoo.elide.async.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
