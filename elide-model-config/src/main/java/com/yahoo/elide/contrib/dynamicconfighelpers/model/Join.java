a5191c001a48e0a8eab3d432fed8d27156ac0644
Rishi Agarwal <rishi.agarwal@verizonmedia.com>
2020-11-10 17:08:26 -0500

    enums should be case insensitive in hjson configs.

--- elide-model-config/src/main/java/com/yahoo/elide/contrib/dynamicconfighelpers/model/Join.java
+++ elide-model-config/src/main/java/com/yahoo/elide/contrib/dynamicconfighelpers/model/Join.java
@@ -5,19 +5,14 @@
  */
 package com.yahoo.elide.contrib.dynamicconfighelpers.model;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import com.fasterxml.jackson.annotation.JsonValue;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Joins describe the SQL expression necessary to join two physical tables.
@@ -36,7 +31,6 @@ import java.util.Map;
 @NoArgsConstructor
 public class Join {
 
-
     @JsonProperty("name")
     private String name;
 
@@ -51,35 +45,18 @@ public class Join {
 
     public enum Type {
 
-        TO_ONE("toOne"),
-        TO_MANY("toMany");
-        private final String value;
-        private final static Map<String, Join.Type> CONSTANTS = new HashMap<String, Join.Type>();
+        TOONE("toOne"),
+        TOMANY("toMany");
 
-        static {
-            for (Join.Type c: values()) {
-                CONSTANTS.put(c.value, c);
-            }
-        }
+        private final String value;
 
         private Type(String value) {
             this.value = value;
         }
 
-        @JsonValue
         @Override
         public String toString() {
             return this.value;
-        }
-
-        @JsonCreator
-        public static Join.Type fromValue(String value) {
-            Join.Type constant = CONSTANTS.get(value);
-            if (constant == null) {
-                throw new IllegalArgumentException(value);
-            } else {
-                return constant;
-            }
         }
     }
 }
