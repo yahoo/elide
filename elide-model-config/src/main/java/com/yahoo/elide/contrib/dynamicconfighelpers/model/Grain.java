b5bd8615e26161016ffbe69646de87d8a6be8e97
rishi-aga <61517466+rishi-aga@users.noreply.github.com>
2020-11-11 17:33:31 -0500

    enums should be case insensitive in hjson configs. (#1657)
    Co-authored-by: Rishi Agarwal <rishi.agarwal@verizonmedia.com>

--- elide-model-config/src/main/java/com/yahoo/elide/contrib/dynamicconfighelpers/model/Grain.java
+++ elide-model-config/src/main/java/com/yahoo/elide/contrib/dynamicconfighelpers/model/Grain.java
@@ -5,19 +5,14 @@
  */
 package com.yahoo.elide.contrib.dynamicconfighelpers.model;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import com.fasterxml.jackson.annotation.JsonValue;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Grain can have SQL expressions that can substitute column
@@ -54,32 +49,14 @@ public class Grain {
         YEAR("YEAR");
 
         private final String value;
-        private final static Map<String, Grain.GrainType> CONSTANTS = new HashMap<String, Grain.GrainType>();
-
-        static {
-            for (Grain.GrainType c: values()) {
-                CONSTANTS.put(c.value, c);
-            }
-        }
 
         private GrainType(String value) {
             this.value = value;
         }
 
-        @JsonValue
         @Override
         public String toString() {
             return this.value;
-        }
-
-        @JsonCreator
-        public static Grain.GrainType fromValue(String value) {
-            Grain.GrainType constant = CONSTANTS.get(value);
-            if (constant == null) {
-                throw new IllegalArgumentException(value);
-            } else {
-                return constant;
-            }
         }
     }
 }
