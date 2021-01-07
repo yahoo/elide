448e4b1ba2b91e35b30564e14c4711ce24c1ad13
rishi-aga <rishi.aga123@gmail.com>
2021-01-07 11:13:20 -0500

    EntityDictionary test

---
+++ elide-core/src/test/java/example/CoerceBean.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2020, Yahoo Inc.
+ * Licensed under the Apache License, Version 2.0
+ * See LICENSE file in project root for terms.
+ */
+package example;
+
+import com.yahoo.elide.annotation.Include;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.persistence.Entity;
+
+@Entity
+@Include(rootLevel = false)
+public class CoerceBean {
+    public String string;
+    public List<Boolean> list;
+    public Map<String, Long> map;
+    public Set<Double> set;
+}
