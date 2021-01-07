ade8e05d340c9d72bda70bfcb4cce119cae14c32
moiz arafat <moiz.arafat@verizonmedia.com>
2021-01-07 18:17:46 -0500

    Review Comments
    Co-authored-by: chandrakaran <chandrakaranreddy.patlolla1@gmail.com>

--- elide-async/src/test/java/com/yahoo/elide/async/service/thread/TableExportThreadTest.java
+++ elide-async/src/test/java/com/yahoo/elide/async/service/thread/TableExportThreadTest.java
@@ -16,6 +16,7 @@ import com.yahoo.elide.async.models.ResultType;
 import com.yahoo.elide.async.models.TableExport;
 import com.yahoo.elide.async.models.TableExportResult;
 import com.yahoo.elide.async.service.storageengine.ResultStorageEngine;
+import com.yahoo.elide.core.PersistentResource;
 
 import org.apache.http.NoHttpResponseException;
 import org.junit.jupiter.api.BeforeEach;
@@ -23,6 +24,7 @@ import org.junit.jupiter.api.Test;
 
 import io.reactivex.Observable;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 
@@ -57,5 +59,47 @@ public class TableExportThreadTest {
         assertEquals(queryResultObj.getRecordCount(), 0);
     }
 
+    @Test
+    public void testJsonToCSV() {
+        String row1 = "id, query, queryType, requestId, principalName, status, createdOn, updatedOn, asyncAfterSeconds, resultType, result";
+        String row2 = "\"edc4a871-dff2-4054-804e-d80075cf827d\", \"/group?sort=commonName&fields%5Bgroup%5D=commonName,description\", \"GRAPHQL_V1_0\"";
+        String row2End = "10.0, \"CSV\", null";
+        TableExport queryObj = new TableExport();
+        String query = "/group?sort=commonName&fields%5Bgroup%5D=commonName,description";
+        String id = "edc4a871-dff2-4054-804e-d80075cf827d";
+        queryObj.setId(id);
+        queryObj.setQuery(query);
+        queryObj.setQueryType(QueryType.GRAPHQL_V1_0);
+        queryObj.setResultType(ResultType.CSV);
+
+        PersistentResource resource = mock(PersistentResource.class);
+        when(resource.getObject()).thenReturn(queryObj);
+        TableExportThread queryThread = new TableExportThread(queryObj, engine, exporter);
+        String output = queryThread.convertToCSV(resource);
+        assertEquals(true, output.startsWith(row1));
+        assertEquals(true, output.contains(row2));
+        assertEquals(true, output.endsWith(row2End));
+    }
+
+    @Test
+    public void testResourceToJSON() throws IOException {
+        String start = "{\"id\":\"edc4a871-dff2-4054-804e-d80075cf827d\",\"query\":\"/group?sort=commonName&fields%5Bgroup%5D=commonName,description\","
+                + "\"queryType\":\"GRAPHQL_V1_0\",";
+        String end = "\"asyncAfterSeconds\":10,\"resultType\":\"CSV\",\"result\":null}";
+        TableExport queryObj = new TableExport();
+        String query = "/group?sort=commonName&fields%5Bgroup%5D=commonName,description";
+        String id = "edc4a871-dff2-4054-804e-d80075cf827d";
+        queryObj.setId(id);
+        queryObj.setQuery(query);
+        queryObj.setQueryType(QueryType.GRAPHQL_V1_0);
+        queryObj.setResultType(ResultType.CSV);
+
+        PersistentResource resource = mock(PersistentResource.class);
+        when(resource.getObject()).thenReturn(queryObj);
+        TableExportThread queryThread = new TableExportThread(queryObj, engine, exporter);
+        String output = queryThread.resourceToJsonStr(resource);
+        assertEquals(true, output.startsWith(start));
+        assertEquals(true, output.endsWith(end));
+    }
     //TODO - Additional Test Cases
 }
