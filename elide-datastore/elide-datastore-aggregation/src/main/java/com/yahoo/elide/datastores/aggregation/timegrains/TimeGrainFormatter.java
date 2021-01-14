abeae82b901a3dd7e790fa36add0318c3c2c0ecf
amakwana <avani.makwana@oath.com>
2021-01-14 00:17:10 -0500

    formt

--- elide-datastore/elide-datastore-aggregation/src/main/java/com/yahoo/elide/datastores/aggregation/timegrains/TimeGrainFormatter.java
+++ elide-datastore/elide-datastore-aggregation/src/main/java/com/yahoo/elide/datastores/aggregation/timegrains/TimeGrainFormatter.java
@@ -12,7 +12,7 @@ import java.text.SimpleDateFormat;
 /** Interface for ISO timegrain Support. */
 public interface TimeGrainFormatter {
 
-    String ISO_FORMAT = "yyyy-MM-dd'T'HH:mm:ssZ";
+    String ISO_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'";
     SimpleDateFormat ISO_FORMATTER = new SimpleDateFormat(ISO_FORMAT);
 
     static Timestamp formatDateString(SimpleDateFormat formatter, String val) throws ParseException {
