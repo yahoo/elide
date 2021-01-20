9f2f632b6ba46ee22db3082554169f583b91be91
amakwana <avani.makwana@oath.com>
2021-01-20 15:43:53 -0500

    unittest

--- elide-spring/elide-spring-boot-autoconfigure/src/test/java/example/tests/DisableGraphQLAsyncTest.java
+++ elide-spring/elide-spring-boot-autoconfigure/src/test/java/example/tests/DisableGraphQLAsyncTest.java
@@ -13,85 +13,49 @@ import static com.yahoo.elide.test.jsonapi.JsonApiDSL.id;
 import static com.yahoo.elide.test.jsonapi.JsonApiDSL.resource;
 import static com.yahoo.elide.test.jsonapi.JsonApiDSL.type;
 import static io.restassured.RestAssured.given;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.hamcrest.Matchers.equalTo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlMergeMode;
 
-import io.restassured.response.Response;
-
-import javax.ws.rs.core.MediaType;
-
 /**
  * Executes Async tests with Aggregation Store disabled.
  */
 @SqlMergeMode(SqlMergeMode.MergeMode.MERGE)
 @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD,
-        statements = "INSERT INTO ArtifactGroup (name, commonName, description, deprecated) VALUES\n"
-                + "\t\t('com.example.repository','Example Repository','The code for this project', false);")
+statements = "INSERT INTO ArtifactGroup (name, commonName, description, deprecated) VALUES\n"
+        + "\t\t('com.example.repository','Example Repository','The code for this project', false);")
 @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD,
-        statements = "DELETE FROM ArtifactVersion; DELETE FROM ArtifactProduct; DELETE FROM ArtifactGroup;")
+statements = "DELETE FROM ArtifactVersion; DELETE FROM ArtifactProduct; DELETE FROM ArtifactGroup;")
 
 @ActiveProfiles("disableGraphQL")
 public class DisableGraphQLAsyncTest extends IntegrationTest {
 
     @Test
     public void testAsyncGraphQL() throws InterruptedException {
+        String expected = "{\"errors\":[{\"detail\":\"Invalid operation: GraphQL is disabled. Please enable GraphQL in settings.\"}]}";
         //Create Async Request
         given()
-                .contentType(JSONAPI_CONTENT_TYPE)
-                .body(
-                        data(
-                                resource(
-                                        type("asyncQuery"),
-                                        id("ba31ca4e-ed8f-4be0-a0f3-12088fa9263d"),
-                                        attributes(
-                                                attr("query", "/group"),
-                                                attr("queryType", "JSONAPI_V1_0"),
-                                                attr("status", "QUEUED"),
-                                                attr("asyncAfterSeconds", "10")
+        .contentType(JSONAPI_CONTENT_TYPE)
+        .body(
+                data(
+                        resource(
+                                type("asyncQuery"),
+                                id("ba31ca4e-ed8f-4be0-a0f3-12088fa9263d"),
+                                attributes(
+                                        attr("query", "/group"),
+                                        attr("queryType", "GRAPHQL_V1_0"),
+                                        attr("status", "QUEUED"),
+                                        attr("asyncAfterSeconds", "10")
                                         )
                                 )
                         ).toJSON())
-                .when()
-                .post("/json/asyncQuery")
-                .then()
-                .statusCode(org.apache.http.HttpStatus.SC_CREATED);
-
-        int i = 0;
-        while (i < 1000) {
-            Thread.sleep(10);
-            Response response = given()
-                    .accept("application/vnd.api+json")
-                    .get("/json/asyncQuery/ba31ca4e-ed8f-4be0-a0f3-12088fa9263d");
-
-            String outputResponse = response.jsonPath().getString("data.attributes.status");
-
-             //If Async Query is created and completed then validate results
-            if (outputResponse.equals("COMPLETE")) {
-
-                try {
-                    // Validate GraphQL Response
-                    String responseGraphQL = given()
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .accept(MediaType.APPLICATION_JSON)
-                            .body("{\"query\":\"{ asyncQuery(ids: [\\\"ba31ca4e-ed8f-4be0-a0f3-12088fa9263d\\\"]) "
-                                    + "{ edges { node { id queryType status result "
-                                    + "{ responseBody httpStatus contentLength } } } } }\","
-                                    + "\"variables\":null }")
-                            .post("/graphql")
-                            .asString();
-                } catch (Exception e) {
-                    assertEquals("GraphQL is disabled. Please enable GraphQL in settings.", e.getMessage());
-                }
-                break;
-            } else if (!(outputResponse.equals("PROCESSING"))) {
-                fail("Async Query has failed.");
-                break;
-            }
-        }
+        .when()
+        .post("/json/asyncQuery")
+        .then()
+        .statusCode(org.apache.http.HttpStatus.SC_BAD_REQUEST)
+        .body(equalTo(expected));
     }
 }
