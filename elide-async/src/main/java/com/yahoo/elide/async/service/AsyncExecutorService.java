46bdc255f5725fd98a14dd521701ebf0fc31397f
rishi-aga <rishi.aga123@gmail.com>
2021-02-11 10:37:31 -0500

    Fix checkstyle

--- elide-async/src/main/java/com/yahoo/elide/async/service/AsyncExecutorService.java
+++ elide-async/src/main/java/com/yahoo/elide/async/service/AsyncExecutorService.java
@@ -23,7 +23,6 @@ import java.util.Map;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
