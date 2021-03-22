4f2e840ed311fe7397e6dc73adc7c174e5e6be80
vandanabhandari <vandana.premkumar@gmail.com>
2021-03-22 17:04:43 -0400

    Fixing the warnings of elide-graphql sub module

--- elide-integration-tests/src/test/java/example/models/generics/package-info.java
+++ elide-integration-tests/src/test/java/example/models/generics/package-info.java
@@ -4,4 +4,9 @@
  * See LICENSE file in project root for terms.
  */
 
+/*
+ * Copyright 2015, Yahoo Inc.
+ * Licensed under the Apache License, Version 2.0
+ * See LICENSE file in project root for terms.
+ */
 package example.models.generics;
