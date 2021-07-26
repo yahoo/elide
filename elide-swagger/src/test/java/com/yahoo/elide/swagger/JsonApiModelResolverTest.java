bca07bcf3aa10949ce5e86831ca27045f49aa2cf
Aaron Klish <aklish@gmail.com>
2021-07-26 10:38:50 -0500

    Resolves #2203 (#2206)
    Co-authored-by: Aaron Klish <klish@verizonmedia.com>

--- elide-swagger/src/test/java/com/yahoo/elide/swagger/JsonApiModelResolverTest.java
+++ elide-swagger/src/test/java/com/yahoo/elide/swagger/JsonApiModelResolverTest.java
@@ -76,6 +76,15 @@ public class JsonApiModelResolverTest {
     }
 
     @Test
+    public void testModelDescriptions() {
+        Resource model = getModel(KEY_BOOK);
+        assertEquals("A book", model.getDescription());
+
+        model = getModel(KEY_AUTHOR);
+        assertNull(model.getDescription());
+    }
+
+    @Test
     public void testModelResolution() {
         ObjectProperty attributes = getObjectProperty(KEY_PUBLISHER, "attributes");
         ObjectProperty relationships = getObjectProperty(KEY_PUBLISHER, "relationships");
