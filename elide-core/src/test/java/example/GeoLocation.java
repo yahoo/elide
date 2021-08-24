8aedda1bd420ef384469acfff4bd64d8632b9b37
Aaron Klish <klish@verizonmedia.com>
2021-08-24 14:34:22 -0500

    Added nested complex attribute to Attribute: GeoLocation

---
+++ elide-core/src/test/java/example/GeoLocation.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2021, Yahoo Inc.
+ * Licensed under the Apache License, Version 2.0
+ * See LICENSE file in project root for terms.
+ */
+package example;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class GeoLocation {
+
+    @Getter @Setter
+    private String latitude;
+
+    @Getter @Setter
+    private String longitude;
+
+}
