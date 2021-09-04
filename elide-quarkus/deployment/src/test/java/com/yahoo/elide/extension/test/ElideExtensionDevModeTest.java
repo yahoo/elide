43d7e14615be88644e22180aedc7d4df08f3ec3e
Aaron Klish <klish@verizonmedia.com>
2021-09-03 22:08:49 -0500

    Commented out dev mode test which doesn't source application properties

--- elide-quarkus/deployment/src/test/java/com/yahoo/elide/extension/test/ElideExtensionDevModeTest.java
+++ elide-quarkus/deployment/src/test/java/com/yahoo/elide/extension/test/ElideExtensionDevModeTest.java
@@ -11,11 +11,11 @@ import io.quarkus.test.QuarkusDevModeTest;
 public class ElideExtensionDevModeTest {
 
     // Start hot reload (DevMode) test with your extension loaded
-    @RegisterExtension
-    static final QuarkusDevModeTest devModeTest = new QuarkusDevModeTest()
-        .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class));
+    //@RegisterExtension
+    //static final QuarkusDevModeTest devModeTest = new QuarkusDevModeTest()
+    //    .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class));
 
-    @Test
+    //@Test
     public void writeYourOwnDevModeTest() {
         // Write your dev mode tests here - see the testing extension guide https://quarkus.io/guides/writing-extensions#testing-hot-reload for more information
         Assertions.assertTrue(true, "Add dev mode assertions to " + getClass().getName());
