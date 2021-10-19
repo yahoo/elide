0fbfdeebde2c4692d9dd720b3d630fd622a34705
Aaron Klish <aklish@gmail.com>
2021-10-19 12:19:37 -0500

    Update Publisher.java

--- elide-datastore/elide-datastore-jms/src/test/java/example/Publisher.java
+++ elide-datastore/elide-datastore-jms/src/test/java/example/Publisher.java
@@ -6,17 +6,12 @@
 
 package example;
 
-import static example.checks.InternalBookCheck.HIDDEN_BOOK;
 import com.yahoo.elide.annotation.Include;
-import com.yahoo.elide.annotation.ReadPermission;
 import com.yahoo.elide.graphql.subscriptions.annotations.Subscription;
 import com.yahoo.elide.graphql.subscriptions.annotations.SubscriptionField;
 import lombok.Data;
 
-import java.util.Set;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
 
 @Include
 @Data
