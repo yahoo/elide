db1dfa18c4d069f4457ea1b0287da0a843583404
Aaron Klish <aklish@gmail.com>
2022-10-23 09:50:33 -0500

    Added unit test.

---
+++ elide-core/src/test/java/com/yahoo/elide/jsonapi/document/processors/PopulateMetaProcessorTest.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2022, Yahoo Inc.
+ * Licensed under the Apache License, Version 2.0
+ * See LICENSE file in project root for terms.
+ */
+
+package com.yahoo.elide.jsonapi.document.processors;
+
+import com.yahoo.elide.core.PersistentResource;
+import com.yahoo.elide.core.RequestScope;
+import com.yahoo.elide.jsonapi.models.Data;
+import com.yahoo.elide.jsonapi.models.JsonApiDocument;
+import com.yahoo.elide.jsonapi.models.Meta;
+import com.yahoo.elide.jsonapi.models.Resource;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class PopulateMetaProcessorTest {
+
+    @Test
+    public void testRequestScopeWithMeta() {
+        RequestScope scope = mock(RequestScope.class);
+        when(scope.getMetadataFields()).thenReturn(Set.of("foo"));
+        when(scope.getMetadataField(eq("foo"))).thenReturn(Optional.of("bar"));
+
+        PersistentResource persistentResource = mock(PersistentResource.class);
+        when(persistentResource.getRequestScope()).thenReturn(scope);
+
+        PopulateMetaProcessor metaProcessor = new PopulateMetaProcessor();
+        JsonApiDocument doc = new JsonApiDocument();
+        metaProcessor.execute(doc, Set.of(persistentResource), null);
+
+        Meta meta = doc.getMeta();
+        assertNotNull(meta);
+        assertEquals(1, meta.getMetaMap().size());
+        assertEquals("bar", meta.getMetaMap().get("foo"));
+    }
+
+    @Test
+    public void testModelWithMeta() {
+        WithMetadata withMetadata = mock(WithMetadata.class);
+        when(withMetadata.getMetadataFields()).thenReturn(Set.of("foo"));
+        when(withMetadata.getMetadataField(eq("foo"))).thenReturn(Optional.of("bar"));
+
+        PersistentResource persistentResource = mock(PersistentResource.class);
+        when(persistentResource.getObject()).thenReturn(withMetadata);
+
+        PopulateMetaProcessor metaProcessor = new PopulateMetaProcessor();
+        JsonApiDocument doc = new JsonApiDocument();
+        Resource resource = new Resource("test", "1");
+        doc.setData(new Data(resource));
+
+        metaProcessor.execute(doc, persistentResource, null);
+
+        Meta meta = doc.getData().getSingleValue().getMeta();
+        assertNotNull(meta);
+        assertEquals(1, meta.getMetaMap().size());
+        assertEquals("bar", meta.getMetaMap().get("foo"));
+    }
+}
