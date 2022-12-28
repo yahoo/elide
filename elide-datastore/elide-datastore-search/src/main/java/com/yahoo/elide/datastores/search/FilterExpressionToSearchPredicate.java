f4b94924f8702ed688020cf08a35e1ae4cef3fd0
Aaron Klish <aklish@gmail.com>
2022-12-28 16:19:10 -0600

    More refactoring.

--- elide-datastore/elide-datastore-search/src/main/java/com/yahoo/elide/datastores/search/FilterExpressionToSearchPredicate.java
+++ elide-datastore/elide-datastore-search/src/main/java/com/yahoo/elide/datastores/search/FilterExpressionToSearchPredicate.java
@@ -14,15 +14,9 @@ import com.yahoo.elide.core.filter.expression.OrFilterExpression;
 import com.yahoo.elide.core.filter.predicates.FilterPredicate;
 import com.yahoo.elide.core.type.ClassType;
 import com.google.common.base.Preconditions;
-import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.core.KeywordAnalyzer;
-import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
-import org.apache.lucene.search.Query;
 import org.hibernate.search.engine.search.predicate.SearchPredicate;
 import org.hibernate.search.engine.search.predicate.dsl.SearchPredicateFactory;
-import org.hibernate.search.jpa.FullTextEntityManager;
-import org.hibernate.search.query.dsl.QueryBuilder;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -30,13 +24,13 @@ import java.util.stream.Collectors;
 /**
  * Converts an Elide filter expression into a Lucene Search query.
  */
-public class FilterExpressionToSearchPredicte implements FilterExpressionVisitor<SearchPredicate> {
+public class FilterExpressionToSearchPredicate implements FilterExpressionVisitor<SearchPredicate> {
 
     SearchPredicateFactory predicateFactory;
 
     private Class<?> entityClass;
 
-    public FilterExpressionToSearchPredicte(SearchPredicateFactory predicateFactory, Class<?> entityClass) {
+    public FilterExpressionToSearchPredicate(SearchPredicateFactory predicateFactory, Class<?> entityClass) {
         this.entityClass = entityClass;
         this.predicateFactory = predicateFactory;
     }
@@ -46,31 +40,24 @@ public class FilterExpressionToSearchPredicte implements FilterExpressionVisitor
         Preconditions.checkArgument(filterPredicate.getPath().getPathElements().size() == 1);
         Preconditions.checkArgument(filterPredicate.getEntityType().equals(ClassType.of(entityClass)));
 
-        Analyzer analyzer = new KeywordAnalyzer();
-        QueryParser queryParser = new QueryParser(filterPredicate.getField(), analyzer);
-
         String queryString = "";
 
         List<String> predicateValues = filterPredicate.getValues()
                 .stream()
                 .map(Object::toString)
                 .map(QueryParser::escape)
-                .map(FilterExpressionToSearchPredicte::escapeWhiteSpace)
+                .map(FilterExpressionToSearchPredicate::escapeWhiteSpace)
                 .collect(Collectors.toList());
 
         Operator op = filterPredicate.getOperator();
 
-        boolean lowerCaseTerms = lowerCaseTerms(op);
-
         queryString = buildQueryString(op, predicateValues);
 
-        queryParser.setLowercaseExpandedTerms(lowerCaseTerms);
-
-        try {
-            return queryParser.parse(queryString);
-        } catch (ParseException e) {
-            throw new IllegalStateException(e);
-        }
+        return predicateFactory
+                .simpleQueryString()
+                .field(filterPredicate.getField())
+                .matching(queryString)
+                .toPredicate();
     }
 
     @Override
@@ -93,17 +80,6 @@ public class FilterExpressionToSearchPredicte implements FilterExpressionVisitor
         return predicateFactory.bool()
                 .mustNot(expression.getNegated().accept(this))
                 .toPredicate();
-    }
-
-    private boolean lowerCaseTerms(Operator op) {
-        switch (op) {
-            case INFIX_CASE_INSENSITIVE:
-            case PREFIX_CASE_INSENSITIVE:
-                return true;
-
-            default:
-                return false;
-        }
     }
 
     private String buildQueryString(Operator op, List<String> predicateValues) {
