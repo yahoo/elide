a42f0cd10dc161e41cc9bdee34c623b9e19b3e34
Justin Tay <justin.tay.tw@gmail.com>
2023-04-26 14:38:58 +0800

    Add tests for json api atomic operations mapper

---
+++ elide-core/src/test/java/com/yahoo/elide/jsonapi/extensions/JsonApiAtomicOperationsMapperTest.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2023, the original author or authors.
+ * Licensed under the Apache License, Version 2.0
+ * See LICENSE file in project root for terms.
+ */
+package com.yahoo.elide.jsonapi.extensions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.yahoo.elide.jsonapi.models.JsonApiDocument;
+import com.yahoo.elide.jsonapi.models.Operations;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests for JsonApiAtomicOperationsMapper.
+ */
+class JsonApiAtomicOperationsMapperTest {
+
+    @Test
+    void readDataCollection() throws JsonProcessingException {
+        JsonApiAtomicOperationsMapper mapper = new JsonApiAtomicOperationsMapper(new ObjectMapper());
+        String operationsDoc = """
+               {
+                  "atomic:operations": [{
+                    "op": "update",
+                    "ref": {
+                      "type": "articles",
+                      "id": "1",
+                      "relationship": "tags"
+                    },
+                    "data": [
+                      { "type": "tags", "id": "2" },
+                      { "type": "tags", "id": "3" }
+                    ]
+                  }]
+                }
+                            """;
+        Operations operations = mapper.readDoc(operationsDoc);
+        JsonApiDocument document = mapper.readData(operations.getOperations().get(0).getData());
+        assertEquals(2, document.getData().get().size());
+    }
+
+    @Test
+    void readDataSingle() throws JsonProcessingException {
+        JsonApiAtomicOperationsMapper mapper = new JsonApiAtomicOperationsMapper(new ObjectMapper());
+        String operationsDoc = """
+                {
+                  "atomic:operations": [{
+                    "op": "update",
+                    "ref": {
+                      "type": "articles",
+                      "id": "13",
+                      "relationship": "author"
+                    },
+                    "data": {
+                      "type": "people",
+                      "id": "9"
+                    }
+                  }]
+                }
+                               """;
+        Operations operations = mapper.readDoc(operationsDoc);
+        JsonApiDocument document = mapper.readData(operations.getOperations().get(0).getData());
+        assertEquals(1, document.getData().get().size());
+    }
+}
