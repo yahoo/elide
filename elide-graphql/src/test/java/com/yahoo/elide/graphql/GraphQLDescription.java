3a956fcd9deca982f9134ec0be6f69ed32842038
Justin Tay <49700559+justin-tay@users.noreply.github.com>
2024-05-20 16:49:44 +0800

    Allow customization of GraphQLFieldDefinition

---
+++ elide-graphql/src/test/java/com/yahoo/elide/graphql/GraphQLDescription.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2024, the original author or authors.
+ * Licensed under the Apache License, Version 2.0
+ * See LICENSE file in project root for terms.
+ */
+package com.yahoo.elide.graphql;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ ElementType.TYPE, ElementType.METHOD, ElementType.PARAMETER, ElementType.FIELD })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface GraphQLDescription {
+    String value() default "";
+}
